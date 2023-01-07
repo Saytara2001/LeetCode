b10f1d23b65545bb3fc23a17874095afbf0fee7f
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-01-07 21:17:33 +0200

    Time: 106 ms (60.66%), Space: 69.5 MB (12.90%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int st = 0, n = gas.size(), sum = 0, total = 0;
+        for(int i = 0; i < n; i++){
+            
+            sum += gas[i] - cost[i];
+            
+            if(sum < 0){
+                total += sum;
+                sum = 0;
+                st = i+1;
+            }
+        }
+        total += sum;
+        return total >= 0 ? st : -1;
+    }
+};
