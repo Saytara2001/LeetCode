07a01c267bf5a125ea48a490709d40644c05d1f1
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-01-20 03:19:09 +0200

    Time: 99 ms (51.40%), Space: 22.2 MB (76.03%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -4,15 +4,17 @@ public:
     vector<int> v;
     void rec(int i, vector<int>& nums) {
         if(i == nums.size()){
-            if(v.size() > 1 and !set.count(v) and is_sorted(v.begin(), v.end())){
+            if(v.size() > 1 and !set.count(v)){
                 set.insert(v);
             }
             return;
         }
         //take
-        v.push_back(nums[i]);
-        rec(i+1, nums);
-        v.pop_back();
+        if(v.empty() or v.back() <= nums[i]){
+            v.push_back(nums[i]);
+            rec(i+1, nums);
+            v.pop_back();
+        }
         //leave
         rec(i+1, nums);
     }
