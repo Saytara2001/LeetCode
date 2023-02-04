7ba47a80ae6b3226c936e147b2f52ffda7b802b2
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-02-04 11:18:04 +0200

    Time: 2041 ms (5.02%), Space: 58.2 MB (6.66%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        unordered_map<string, bool> mp;
         sort(s1.begin(), s1.end());
         for(int i = 0; i+s1.size() <= s2.size(); i++) {
             string tmp = s2.substr(i, s1.size());
             sort(tmp.begin(), tmp.end());
-            mp[tmp] = 1;
-            if(mp[s1]) return true;
+            if(tmp == s1) return true;
         }
         return false;
     }
