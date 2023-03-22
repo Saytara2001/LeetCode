b82ba4a8d64601fbd856c377d8628c3fa1728c6e
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-03-22 09:54:21 +0200

    Time: 519 ms (60.00%), Space: 125.1 MB (75.69%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,24 +1,56 @@
-class Solution {
-public:
-    vector<int> vis;
-    int mn = 1e9;
-    void dfs(int node, vector<vector<pair<int, int>>>& adj) {
-        vis[node] = 1;
-        for(auto child:adj[node]) {
-            mn = min(mn, child.second);
-            if(!vis[child.first]) {
-                dfs(child.first, adj);
-            }
+struct DSU {
+    vector<int> parent, sz;
+
+    DSU(int n) {
+        parent.resize(n);
+        sz.resize(n);
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            sz[i] = 1;
         }
     }
+
+    int FindParent(int i) {
+        if (parent[i] == i)
+            return i;
+        return parent[i] = FindParent(parent[i]);
+    }
+
+    bool IsSameSet(int x, int y) {
+        x = FindParent(x);
+        y = FindParent(y);
+        return (x == y);
+    }
+
+    void Connect(int x, int y) {
+        x = FindParent(x);
+        y = FindParent(y);
+        if (x == y)
+            return;
+        if (sz[x] < sz[y])
+            swap(x, y);
+        parent[y] = x;
+        sz[x] += sz[y];
+    }
+
+    int Getsize(int x) {
+        x = FindParent(x);
+        return sz[x];
+    }
+};
+class Solution {
+public:
     int minScore(int n, vector<vector<int>>& roads) {
-        vector<vector<pair<int, int>>> adj(n+1);
-        vis = vector<int> (n+1, 0);
+        DSU dsu(n+1);
         for(auto vv:roads) {
-            adj[vv[0]].push_back({vv[1], vv[2]});
-            adj[vv[1]].push_back({vv[0], vv[2]});
+            dsu.Connect(vv[0], vv[1]);
         }
-        dfs(1, adj);
+        int p = dsu.FindParent(1);
+        int mn = 1e8;
+        for(auto vv:roads) {
+            if(p == dsu.FindParent(vv[0]))
+                mn = min(mn, vv[2]);
+        }
         return mn;
     }
 };
