0d98ed85d9278f2e18e8773b52742dd8691d5f45
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-04-02 07:23:00 +0200

    Time: 996 ms (20.72%), Space: 37.9 MB (28.16%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    // int dp[2501][2501];
-    int rec(int i, int prev, vector<int>& v, vector<vector<int>>& dp) {
+    int dp[2501][2501];
+    int rec(int i, int prev, vector<int>& v) {
         if (i == v.size())
             return 0;
 
         if (dp[i][prev] != -1)
             return dp[i][prev];
 
-        int take = 0;
-        int leave = rec(i + 1, prev, v, dp);
+        int leave = 0, take = INT_MIN;
+        leave = rec(i + 1, prev, v);
         
         if(i == prev) {
-            take = rec(i + 1, i , v, dp) + 1;
-            leave = rec(i + 1, i + 1, v, dp);
+            take = rec(i + 1, i , v) + 1;
+            leave = rec(i + 1, i + 1, v);
         }else if (v[i] > v[prev]) {
-            take = rec(i + 1, i , v, dp) + 1;
+            take = rec(i + 1, i , v) + 1;
         }
         return dp[i][prev] = max(take, leave);
     }
     int lengthOfLIS(vector<int>& nums) {
         
-        // memset(dp, -1, sizeof(dp));
-        int n = nums.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
+        memset(dp, -1, sizeof(dp));
         
-        return rec(0, 0, nums, dp);
+        return rec(0, 0, nums);
     }
 };
