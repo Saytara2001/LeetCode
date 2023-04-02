4b151a79e7b54160e0f1f0f1741e51c9a843acde
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-04-02 05:37:34 +0200

    Time: 238 ms (82.78%), Space: 99.9 MB (51.66%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -4,15 +4,17 @@ public:
         sort(begin(potions), end(potions));
         vector<int> res;
         int n = potions.size();
-        int maxPotion = potions.back();
         for(int i = 0; i < spells.size(); i++) {
             long long need = (success + spells[i] - 1) / spells[i];
-           if(need > maxPotion) {
-               res.push_back(0);
-               continue;
-           }
-            int idx = lower_bound(potions.begin(), potions.end(), need) - potions.begin();
-            res.push_back(n - idx);  
+            int l = 0, r = n;
+            while(l < r) {
+                int mid = (l+r)/2;
+                if(potions[mid] >= need)
+                    r = mid;
+                else
+                    l = mid + 1;
+            } 
+            res.push_back(n - l);
         }
         return res;
     }
