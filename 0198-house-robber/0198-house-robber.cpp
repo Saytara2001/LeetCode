4c3b9f88ce5a42ed2b8d72d2be7c40d625b46e58
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-04-04 01:21:58 +0200

    Time: 0 ms (100.00%), Space: 7.7 MB (62.42%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    int dp[105][105];
+    int dp[105];
     vector<int> v;
-    int rec(int i, int lst) {
+    int rec(int i) {
 
-        if (i == v.size())
+        if (i >= v.size())
             return 0;
 
-        int &ret = dp[i][lst];
+        int &ret = dp[i];
+        
         if (~ret)
             return ret;
 
-        int leave = rec(i + 1, lst);
-        int take = 0;
-
-        if (i - lst > 1 or lst == v.size())
-            take = rec(i + 1, i) + v[i];
+        int leave = rec(i + 1);
+        int take = rec(i + 2) + v[i];
 
         return ret = max(take, leave);
     }
@@ -24,6 +22,6 @@ public:
         for(auto vv:nums) {
             v.push_back(vv);
         }
-        return rec(0, nums.size());
+        return rec(0);
     }
 };
