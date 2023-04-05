71d3a252c5c9c18f7e2b140026d26012d641d1f9
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-04-05 21:15:08 +0200

    Time: 98 ms (8.68%), Space: 7.5 MB (73.42%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,40 +1,33 @@
-class Solution
-{
+class Solution {
 public:
-    vector<vector<int>> memo;
-    int minDistance(string word1, string word2)
-    {
-        memo.resize(word1.length() + 1, vector<int>(word2.length() + 1, -1));
-        return minDistanceRecur(word1, word2, word1.size(), word2.size());
+    int dp[505][505];
+    string s, t;
+    int rec(int i, int j) {
+        
+        if(i == s.size()) // base case 1
+            return t.size() - j;
+        
+        if(j == t.size()) // base case 2
+            return s.size() - i;
+        
+        int &ret = dp[i][j];
+        if(~ret)
+            return ret;
+        
+        if(s[i] == t[j])
+            return rec(i + 1, j + 1);
+        
+        int replace = rec(i + 1, j + 1) + 1;
+        int insert = rec(i + 1, j) + 1;
+        int del = rec(i, j + 1) + 1;
+        
+        return ret = min({replace, insert, del});
     }
-
-    int minDistanceRecur(string &word1, string &word2, int word1Index, int word2Index)
-    {
-        if (word1Index == 0)
-        {
-            return word2Index;
-        }
-        if (word2Index == 0)
-        {
-            return word1Index;
-        }
-        if (memo[word1Index][word2Index] != -1)
-        {
-            return memo[word1Index][word2Index];
-        }
-        int minEditDistance = 0;
-        if (word1[word1Index - 1] == word2[word2Index - 1])
-        {
-            minEditDistance = minDistanceRecur(word1, word2, word1Index - 1, word2Index - 1);
-        }
-        else
-        {
-            int insertOperation = minDistanceRecur(word1, word2, word1Index, word2Index - 1);
-            int deleteOperation = minDistanceRecur(word1, word2, word1Index - 1, word2Index);
-            int replaceOperation = minDistanceRecur(word1, word2, word1Index - 1, word2Index - 1);
-            minEditDistance = min(insertOperation, min(deleteOperation, replaceOperation)) + 1;
-        }
-        memo[word1Index][word2Index] = minEditDistance;
-        return minEditDistance;
+    int minDistance(string word1, string word2) {
+        s = word1; t = word2;
+        
+        memset(dp, -1, sizeof dp);
+        
+        return rec(0, 0);
     }
 };
