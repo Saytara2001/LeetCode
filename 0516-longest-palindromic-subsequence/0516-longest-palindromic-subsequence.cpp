28af740962ad982ac97cea0e04182a320cbab998
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-04-15 00:13:26 +0200

    Time: 58 ms (97.58%), Space: 10.8 MB (79.47%) - LeetHub

---
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int dp[1005][1005];
+    int rec(int l, int r, string& s) {
+        
+        if(l == r)
+            return 1;
+        
+        if(l > r)
+            return 0;
+        
+        int &ret = dp[l][r];
+        
+        if(~ret)
+            return ret;
+        
+        if(s[l] == s[r]) {
+            ret = max(ret, rec(l + 1, r - 1, s) + 2);
+        }else {
+            ret = max(ret, rec(l + 1, r, s));
+            ret = max(ret, rec(l, r - 1, s));
+        }
+        
+        return ret;
+    }
+    int longestPalindromeSubseq(string s) {
+        
+        memset(dp, -1, sizeof dp);
+        return rec(0, s.size() - 1, s);
+    }
+};
