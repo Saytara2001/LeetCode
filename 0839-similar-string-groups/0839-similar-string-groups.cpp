c416dad33818ca5255ab8c25a77a3dbc3c97b63f
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-04-28 16:56:59 +0200

    Time: 511 ms (14.60%), Space: 10 MB (94.29%) - LeetHub

---
+++ 0839-similar-string-groups/0839-similar-string-groups.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    struct DSU {
+        vector<int> parent, sz;
+
+        DSU(int n) {
+            parent.resize(n);
+            sz.resize(n);
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                sz[i] = 1;
+            }
+        }
+
+        int FindParent(int i) {
+            if (parent[i] == i)
+                return i;
+            return parent[i] = FindParent(parent[i]);
+        }
+
+        bool IsSameSet(int x, int y) {
+            x = FindParent(x);
+            y = FindParent(y);
+            return (x == y);
+        }
+
+        bool Connect(int x, int y) {
+            x = FindParent(x);
+            y = FindParent(y);
+            if (x == y)
+                return 0;
+            if (sz[x] < sz[y])
+                swap(x, y);
+            parent[y] = x;
+            sz[x] += sz[y];
+            return 1;
+        }
+
+        int Getsize(int x) {
+            x = FindParent(x);
+            return sz[x];
+        }
+    };
+    int numSimilarGroups(vector<string>& v) {
+        int n = v.size(), comp = n;
+        DSU dsu(n);
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                int diff = 0;
+                if(dsu.FindParent(i) != dsu.FindParent(j)) {
+                    for (int k = 0; k < v[i].size(); ++k) {
+                            if (v[i][k] != v[j][k]) {
+                                diff++;
+                            }
+                    }
+                    if (diff == 2 or diff == 0) {
+                        if(dsu.Connect(i, j))
+                            comp--;
+                    }
+                }
+            }
+        }
+        return comp;
+    }
+};
