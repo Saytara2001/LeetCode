a9bdfdd99a188a1e84342bd598f5bc2671049982
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-05-13 19:11:01 +0300

    Time: 17 ms (61.03%), Space: 10.1 MB (68.77%) - LeetHub

---
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    long long dp[100005];
+    long long mod = 1000000007;
+    long long rec(int rem, int zero, int one) {
+        
+        long long &ret = dp[rem];
+        
+        if(~ret)
+            return ret;
+        
+        long long count = 0;
+        
+        if(rem >= one) {
+            count += rec(rem - one, zero, one);
+        }
+        
+        if(rem >= zero) {
+            count += rec(rem - zero, zero, one);
+        }
+        ret = count % mod;
+        return ret;
+    }
+    
+    int countGoodStrings(int low, int high, int zero, int one) {
+        memset(dp, -1, sizeof dp);
+        dp[0] = 1;
+        long long ans = 0;
+        for(int i = low; i <= high; i++) {
+            ans += rec(i, zero, one);
+            ans %= mod;
+        }
+        return ans;
+    }
+};
