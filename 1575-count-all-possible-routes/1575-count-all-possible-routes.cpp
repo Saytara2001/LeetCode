064acc523340041b7d087ad849509a3bc7e1b5e2
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-06-25 14:40:12 +0300

    Time: 150 ms (94.48%), Space: 8.7 MB (84.25%) - LeetHub

--- 1575-count-all-possible-routes/1575-count-all-possible-routes.cpp
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.cpp
@@ -7,24 +7,20 @@ public:
     int dp[101][201], st, end;
     int rec(int i, int fuel, vector<int>& locations) {
         
-        // if(i == end)
-        //     return 1;
-        
-        
         int &ret = dp[i][fuel];
         if(~ret)
             return ret;
         
-        int ans = i == end ? 1 : 0;
-        // ret = 0;
+        ret = i == end ? 1 : 0;
+        // ret = add(ret, rec(i + 1, fuel, locations));
+        
         for(int k = 0; k < size(locations); k++) {
             int dis = abs(locations[k] - locations[i]);
             if(k != i and dis <= fuel) {
-                // ret = add(ret, rec(i, fuel, locations)); //leave
-                ans = add(ans, rec(k, fuel - dis, locations)); //take
+                ret = add(ret, rec(k, fuel - dis, locations));
             }
         }
-        return ret = ans;
+        return ret;
     }
     int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
         st = start, end = finish;
