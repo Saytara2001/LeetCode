afe7241650ccbc16a0a5abec18c2f523babf1fb6
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-06-26 13:23:36 +0300

    Time: 340 ms (13.51%), Space: 111.4 MB (5.23%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     long long totalCost(vector<int>& costs, int k, int candidates) {
-        priority_queue<int> q1, q2;
+        multiset<int> st1, st2;
         int l = 0, r = size(costs) - 1;
         long long ans = 0;
         while(k--) {
             while (l <= r) {
-                if(size(q1) < candidates) {
-                    q1.push(-costs[l++]);
-                }else if(size(q2) < candidates) {
-                    q2.push(-costs[r--]);
+                if(size(st1) < candidates) {
+                    st1.insert(costs[l++]);
+                }else if(size(st2) < candidates) {
+                    st2.insert(costs[r--]);
                 }else
                     break;
             }
-            if(q2.empty() or (!q1.empty() and -q1.top() <= -q2.top())) {
-                ans += -q1.top();
-                q1.pop();
+            if(st2.empty() or (!st1.empty() and *st1.begin() <= *st2.begin())) {
+                ans += *st1.begin();
+                st1.erase(st1.begin());
             }
             else {
-                ans += -q2.top();
-                q2.pop();
+                ans += *st2.begin();
+                st2.erase(st2.begin());
             }
         }
         return ans;
