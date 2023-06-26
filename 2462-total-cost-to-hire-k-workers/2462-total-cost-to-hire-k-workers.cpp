e42174a48e0a9a2321523e2d9ec3121e9276088b
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-06-26 13:19:16 +0300

    Time: 189 ms (91.19%), Space: 72.7 MB (82.21%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     long long totalCost(vector<int>& costs, int k, int candidates) {
-        priority_queue<int,vector<int>,greater<int>> q1, q2;
+        priority_queue<int> q1, q2;
         int l = 0, r = size(costs) - 1;
         long long ans = 0;
         while(k--) {
             while (l <= r) {
                 if(size(q1) < candidates) {
-                    q1.push(costs[l++]);
+                    q1.push(-costs[l++]);
                 }else if(size(q2) < candidates) {
-                    q2.push(costs[r--]);
+                    q2.push(-costs[r--]);
                 }else
                     break;
             }
-            if(q2.empty() or (!q1.empty() and q1.top() <= q2.top())) {
-                ans += q1.top();
+            if(q2.empty() or (!q1.empty() and -q1.top() <= -q2.top())) {
+                ans += -q1.top();
                 q1.pop();
             }
             else {
-                ans += q2.top();
+                ans += -q2.top();
                 q2.pop();
             }
         }
