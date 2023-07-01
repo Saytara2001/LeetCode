45035405fa7d2662157eb835aac1719399a9bf76
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-07-01 17:10:17 +0300

    Time: 1776 ms (36.83%), Space: 6.8 MB (62.54%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int mn = 1e9, n, kk;
     vector<int> sum;
-    void rec(int i, vector<int>& cookies) {
+    int rec(int i, vector<int>& cookies) {
         if(i == n) {
             int mx = 0;
             for(auto vv: sum)
                 mx = max(mx, vv);
-            mn = min(mn, mx);
-            return;
+            mn = min(mn, mx);;
+            return mn;
         }
         
         for(int j = 0; j < kk; j++) {
@@ -16,12 +16,12 @@ public:
             rec(i + 1, cookies);
             sum[j] -= cookies[i];
         }
+        return mn;
     }
     int distributeCookies(vector<int>& cookies, int k) {
         n = size(cookies);
         kk = k;
         sum = vector<int> (n);
-        rec(0, cookies);
-        return mn;
+        return rec(0, cookies);
     }
 };
