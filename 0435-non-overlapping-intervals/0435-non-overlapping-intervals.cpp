5ff5dcbf4ce7e8c0303fc65303e4b5df3343b6ed
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-07-19 14:07:11 +0300

    Time: 474 ms (74.36%), Space: 89.8 MB (58.83%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -3,17 +3,15 @@ public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         sort(begin(intervals), end(intervals));
         int ans = 0;
-        int lst_interval = 0;
+        int mnEnd = intervals[0][1];
         for(int i = 1 ; i < size(intervals); i++) {
             //second start less than first end
-            if(intervals[i][0] < intervals[lst_interval][1]) {
+            if(intervals[i][0] < mnEnd) {
                 //look at whose has minimun end
-                if(intervals[i][1] < intervals[lst_interval][1]) {
-                    lst_interval = i;
-                }
+                mnEnd = min(mnEnd, intervals[i][1]);
                 ans++;
             }else
-                lst_interval = i;
+                mnEnd = intervals[i][1];
         }
         return ans;
     }
