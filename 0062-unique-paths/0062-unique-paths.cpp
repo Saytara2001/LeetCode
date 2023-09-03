4fbf89b2ece81a684dde11d76fd1ec9977a323b7
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-09-03 10:35:35 +0300

    Time: 0 ms (100.00%), Space: 6 MB (88.67%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
+    int nn, mm, dp[101][101];
+    int rec(int i, int j) {
+        if(i >= nn or j >= mm)
+            return 0;
+        if(i == nn-1 or j == mm-1)
+            return 1;
+        
+        int &ret = dp[i][j];
+        if(~ret)
+            return ret;
+        
+        ret = rec(i + 1, j);
+        ret += rec(i, j + 1);
+        
+        return ret;
+    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(n, vector<int> (m));
-        for(int i = 0 ; i < n ; i++) {
-            for(int j = 0 ;j < m; j++) {
-                if(i == 0 or j == 0) {
-                    dp[i][j] = 1;
-                    continue;
-                }
-                dp[i][j] += dp[i-1][j] + dp[i][j-1];
-            }
-        }
-        return dp[n-1][m-1];
+        nn = n;
+        mm = m;
+        memset(dp, -1, sizeof dp);
+        return rec(0, 0);
     }
 };
