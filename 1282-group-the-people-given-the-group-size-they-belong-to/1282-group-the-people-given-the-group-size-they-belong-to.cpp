a4cd4ce126088f79aa0d2266ed8e5390eea9388e
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-09-11 12:52:37 +0300

    Time: 8 ms (78.38%), Space: 13.2 MB (56.43%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -2,19 +2,13 @@ class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
         int n = size(groupSizes);
+        vector<vector<int>> res;
         vector<vector<int>> v(n + 1);
         for(int i = 0; i < size(groupSizes) ; i++) {
             v[groupSizes[i]].push_back(i);
-        }
-        vector<vector<int>> res;
-        for(int i = 1; i <= n ; i++) {
-            vector<int> peoples;
-            for(int j = 0 ; j < size(v[i]); j++) {
-                peoples.push_back(v[i][j]);
-                if(peoples.size() == i) {
-                    res.push_back(peoples);
-                    peoples.clear();
-                }
+            if(v[groupSizes[i]].size() == groupSizes[i]) {
+                res.push_back(v[groupSizes[i]]);
+                v[groupSizes[i]].clear();
             }
         }
         return res;
