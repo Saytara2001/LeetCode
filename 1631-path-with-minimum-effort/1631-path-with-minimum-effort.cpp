ad825d8f954528d97d4cb69584b1e57487808979
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-09-16 21:27:26 +0300

    Time: 488 ms (8.02%), Space: 69.5 MB (7.61%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -8,12 +8,13 @@ public:
     bool can(int diff, vector<vector<int>>& v) {
         
         queue<pair<int, int>> q;
-        map<pair<int, int>, bool> vis;
-        
-        q.push({0 ,0});
-        vis[{0 ,0}] = 1;
         int n = v.size();
         int m = v[0].size();
+        vector<vector<int>> vis(n, vector<int> (m));
+        
+        q.push({0 ,0});
+        vis[0][0]= 1;
+        
         
         while(q.size()) {
             int i = q.front().first;
@@ -23,13 +24,13 @@ public:
             for(int d = 0; d < 4 ; d++) {
                 int ni = i + dx[d]; 
                 int nj = j + dy[d];
-                if(valid(ni, nj, n, m) and !vis[{ni, nj}] and abs(v[ni][nj] - v[i][j]) <= diff) {
+                if(valid(ni, nj, n, m) and !vis[ni][nj] and abs(v[ni][nj] - v[i][j]) <= diff) {
                     q.push({ni, nj});
-                    vis[{ni, nj}] = 1;
+                    vis[ni][nj] = 1;
                 }
             }
         }
-        return vis[{n - 1, m - 1}];
+        return vis[n - 1][m - 1];
     }
     int minimumEffortPath(vector<vector<int>>& heights) {
         int l = 0, r = 1e6;
