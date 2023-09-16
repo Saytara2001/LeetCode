773f5247d7b6be5727a84ddd007b5525bf50a217
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-09-16 21:23:09 +0300

    Time: 1868 ms (5.01%), Space: 256.5 MB (5.00%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int dx[4] = {-1, 0 , 1, 0},
+        dy[4] = {0, 1 , 0, -1};
+    bool valid(int i, int j, int n, int m) {
+        return (i >= 0 and i < n and j >= 0 and j < m);
+    }
+    bool can(int diff, vector<vector<int>>& v) {
+        
+        queue<pair<int, int>> q;
+        map<pair<int, int>, bool> vis;
+        
+        q.push({0 ,0});
+        vis[{0 ,0}] = 1;
+        int n = v.size();
+        int m = v[0].size();
+        
+        while(q.size()) {
+            int i = q.front().first;
+            int j = q.front().second;
+            q.pop();
+            
+            for(int d = 0; d < 4 ; d++) {
+                int ni = i + dx[d]; 
+                int nj = j + dy[d];
+                if(valid(ni, nj, n, m) and !vis[{ni, nj}] and abs(v[ni][nj] - v[i][j]) <= diff) {
+                    q.push({ni, nj});
+                    vis[{ni, nj}] = 1;
+                }
+            }
+        }
+        return vis[{n - 1, m - 1}];
+    }
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int l = 0, r = 1e6;
+        while(l < r) {
+            int mid = l + (r - l) / 2;
+            if(can(mid, heights)) {
+                r = mid;
+            }else {
+                l = mid + 1;
+            }
+        }
+        return r;
+    }
+};
