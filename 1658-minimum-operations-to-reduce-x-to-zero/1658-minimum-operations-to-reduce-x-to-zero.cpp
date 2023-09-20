1c571b0d45e01abeb2d6cb5159a9fdabfc7bd6f1
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-09-20 18:41:20 +0300

    Time: 131 ms (49.37%), Space: 98.9 MB (36.20%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        
+        int pre = 0, cnt = 0, n = size(nums);
+        int i = 0;
+        for( ;i < n; i++) {
+            if(pre + nums[i] <= x) {
+                pre += nums[i];
+                cnt++;
+            }else {
+                i--;
+                break;
+            }
+        }
+        
+        int mn = 1e9;
+        if(pre == x)
+            mn = min(mn, cnt);
+        
+        int r = n - 1;
+        while(r >= 0 and i < r) {
+            pre += nums[r--];
+            cnt++;
+            while(i >= 0 and pre > x) {
+                pre -= nums[i--];
+                cnt--;
+            }
+            if(pre == x)
+                mn = min(mn, cnt);
+        }
+        return mn == 1e9 ? -1 : mn;
+    }
+};
