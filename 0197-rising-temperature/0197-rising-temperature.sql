591bbd49d10ea3cb4dddb6b16ea2c3f8ffadc5a8
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-09-24 21:08:01 +0300

    Time: 1229 ms (32.68%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,4 +1,4 @@
 /* Write your T-SQL query statement below */
 select distinct W.id
 from Weather W join Weather Wa
-on DATEDIFF(dd, W.recordDate, Wa.recordDate) = -1 and W.temperature > Wa.temperature 
+on DATEDIFF(dd, Wa.recordDate, W.recordDate) = 1 and W.temperature > Wa.temperature 
