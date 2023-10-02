50a72fcc1ad5041dd6a0a5f6a58d5cc57f275469
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-10-02 15:14:38 +0300

    Time: 0 ms (100.00%), Space: 6.5 MB (14.39%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+2);
-        dp[1] = 1;
-        dp[2] = 2;
-        for(int i = 3; i <= n; i++){
-            dp[i] = dp[i-1] + dp[i - 2];
+        vector<int> dp(n + 1);
+        dp[0] = 1; // one choice (1 step) first stair
+        dp[1] = 2; // two choice (2 step) second stair (1 + 1, 2)
+        for(int i = 2 ;i < n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        return dp[n];
+        return dp[n - 1];
     }
 };
