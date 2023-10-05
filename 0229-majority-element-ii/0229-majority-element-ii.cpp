e6eda8c70f1dc2926f7dfc579148b9e2acd56f6b
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-10-05 11:01:20 +0300

    Time: 7 ms (87.90%), Space: 16.2 MB (18.73%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,19 +1,43 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        //n log n
-        vector<int> v;
-        sort(begin(nums), end(nums));
-        int n = nums.size();
-        for(int i = 0; i < n; i++) {
-            int cur = i;
-            while(cur < n and nums[cur] == nums[i]) {
-                cur++;
+        int candidate1 = 0, candidate2 = 0, count1 = 0, count2 = 0;
+        
+        for (int it : nums) {
+            if (it == candidate1) {
+                count1++;
+            } else if (it == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = it;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = it;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
             }
-            if(cur - i > n / 3)
-                v.push_back(nums[i]);
-            i = cur - 1;
         }
-        return v;
+        
+        // Reset counts to 0 to re-count occurrences of the candidates.
+        count1 = count2 = 0;
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            }
+        }
+        
+        vector<int> res;
+        if (count1 > nums.size() / 3) {
+            res.push_back(candidate1);
+        }
+        if (count2 > nums.size() / 3) {
+            res.push_back(candidate2);
+        }
+        
+        return res;
     }
 };
