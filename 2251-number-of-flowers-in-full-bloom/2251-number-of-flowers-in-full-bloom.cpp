42dccaa0bf6a9510c2f07605dfd27bd8cb58804c
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-10-11 07:18:10 +0300

    Time: 602 ms (5.26%), Space: 317.5 MB (5.08%) - LeetHub

---
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
@@ -0,0 +1,38 @@
+// typedef (lower_bound(begin(indices), end(indices), i) - begin(indices)) getCompressedIndex(i)
+class Solution {
+public:
+    
+    vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& people) {
+        vector<int> indices;
+        // auto getCompressedIndex = [](int i) {
+        //     return lower_bound(begin(indices), end(indices), i) - begin(indices);
+        // };
+        vector<int> pref(1000000);
+        for(auto it: flowers) {
+            indices.push_back(it[0]);
+            indices.push_back(it[1]);
+        }
+        for(auto it: people) {
+            indices.push_back(it);
+        }
+        //go compress and prefix
+        sort(indices.begin(), indices.end());
+	    indices.erase(unique(indices.begin(), indices.end()), indices.end());
+        for(auto it: flowers) {
+            int com = lower_bound(begin(indices), end(indices), it[0]) - begin(indices);
+            pref[com]++;
+            com = lower_bound(begin(indices), end(indices), it[1]) - begin(indices);
+            pref[com + 1]--;
+        }
+        for(int i = 1 ; i < 1000000; i++) {
+            pref[i] += pref[i - 1];
+        }
+        
+        vector<int> res;
+        for(auto it: people) {
+            int com = lower_bound(begin(indices), end(indices), it) - begin(indices);
+            res.push_back(pref[com]);
+        }
+        return res;
+    }
+};
