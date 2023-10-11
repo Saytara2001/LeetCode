d7d2f2e97952339c7b6fb5db4bc57647c1a83e16
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-10-11 10:42:46 +0300

    Time: 264 ms (46.64%), Space: 89.3 MB (43.56%) - LeetHub

--- 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
@@ -5,7 +5,7 @@ public:
         vector<int> start, finish;
         for(auto it: flowers) {
             start.push_back(it[0]);
-            finish.push_back(it[1]);
+            finish.push_back(it[1] + 1);
         }
         
         sort(begin(start), end(start));  
@@ -14,7 +14,7 @@ public:
         vector<int> res;
         for(auto it: people) {
             int j = upper_bound(begin(start), end(start), it) - begin(start);
-            int i = lower_bound(begin(finish), end(finish), it) - begin(finish);
+            int i = upper_bound(begin(finish), end(finish), it) - begin(finish);
             res.push_back(j - i);
         }
         return res;
