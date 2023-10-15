0d77b57c10d683b6a6a6e64109d5e4f7b49801a6
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-10-15 23:51:57 +0300

    Time: 15 ms (84.71%), Space: 24.2 MB (71.41%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -5,14 +5,14 @@ public:
         int n = size(nums);
         vector<int> ans(n, 1); 
         int p = 1;
-        for(int i = 0; i + 1 < n; i++) {
+        for(int i = 0; i  < n; i++) {
+            ans[i] = p;
             p *= nums[i];
-            ans[i + 1] = p;
         }
         p = 1;
-        for(int i = n - 1; i > 0; i--) {
+        for(int i = n - 1; i >= 0; i--) {
+            ans[i] *= p;
             p *= nums[i];
-            ans[i - 1] *= p;
         }
         return ans;
     }
