e5ab11567fe69410149e87c467a0012cf6973f27
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-10-18 11:41:58 +0300

    Time: 356 ms (45.86%), Space: 146.3 MB (29.50%) - LeetHub

--- 2050-parallel-courses-iii/2050-parallel-courses-iii.cpp
+++ 2050-parallel-courses-iii/2050-parallel-courses-iii.cpp
@@ -6,30 +6,26 @@ public:
         vector<bool> vis(n + 1);
         vector<int> cost(n + 1);
         queue<int> q;
-        int maxTime = 0;
         for(int i = 1; i <= n; i++) {
             if(!in[i]) {
                 q.push(i);
                 vis[i] = 1;
             }
             cost[i] = time[i - 1];
-            maxTime = max(maxTime, time[i - 1]);
         }
         while(!q.empty()) {
             int node = q.front();
             q.pop();
             
             for(auto child: adj[node]) {
-                if(in[child] > 0) {
-                    if(--in[child] == 0) {
+                if(--in[child] == 0) {
                         q.push(child);
                         vis[child] = 1;
-                    }
-                    cost[child] = max(cost[node] + time[child - 1], cost[child]);
                 }
+                cost[child] = max(cost[node] + time[child - 1], cost[child]);
             }
         }
-        return max(maxTime, *max_element(begin(cost), end(cost)));
+        return *max_element(begin(cost), end(cost));
     }
     int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {
         in = vector<int> (n + 1);
