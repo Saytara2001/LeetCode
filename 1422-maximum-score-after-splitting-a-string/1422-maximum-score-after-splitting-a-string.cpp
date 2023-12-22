a26f0b6be8fd6330c5bf39d533257ce7da2df679
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-12-22 11:33:57 +0200

    Time: 0 ms (100.00%), Space: 6.8 MB (43.75%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -4,7 +4,7 @@ public:
         int zeros = 0;
         int ones = count(begin(s), end(s), '1');
         int score = 0;
-        for(int i = 0; i < size(s)- 1; i++) {
+        for(int i = 0; i < size(s) - 1; i++) {
             s[i] == '0' ? zeros++ : ones--;
             score = max(score, zeros + ones);
         }
