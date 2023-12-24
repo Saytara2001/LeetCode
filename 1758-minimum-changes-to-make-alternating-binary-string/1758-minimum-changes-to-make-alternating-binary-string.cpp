9db4801cf7377d5affea03430e117133fecb7081
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2023-12-24 10:08:06 +0200

    Time: 3 ms (78.34%), Space: 7.2 MB (86.65%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     int minOperations(string s) {
-        int ans = 1e5, cnt = 0;
-        char c = '1';
-        for(auto it: s) {
-            cnt += (c != it);
-            c = '1' - c + '0';
+        int cnt = 0;
+        for(int i = 0; i < size(s); i++) {
+            if(i & 1) {
+                cnt += (s[i] != '1');
+            }else {
+                cnt += (s[i] != '0');
+            }
         }
-        ans = min(ans, cnt);
-        c = '0'; cnt = 0;
-        for(auto it: s) {
-            cnt += (c != it);
-            c = '1' - c + '0';
-        }
-        ans = min(ans, cnt);
-        return ans;
+        return (int) min(cnt, (int) size(s) - cnt);
     }
 };
