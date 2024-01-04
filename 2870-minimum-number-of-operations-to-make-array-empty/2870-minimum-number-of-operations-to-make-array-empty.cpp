ec8f110cfe306b905fd1067f69dc232a6fa91f84
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2024-01-04 04:47:09 -0800

    Time: 119 ms (65.19%), Space: 85 MB (90.06%) - LeetHub

---
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for(auto it: nums) {
+            mp[it]++;
+        }
+        int ans = 0;
+        for(auto it: mp) {
+            if(it.second == 1)
+                return -1;
+            ans += (it.second + 2) / 3;
+        }
+        return ans;
+    }
+};
