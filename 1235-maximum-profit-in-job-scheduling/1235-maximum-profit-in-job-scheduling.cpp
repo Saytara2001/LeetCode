3ef99ef8a145e51bcc2e33d2e1885f0eb99d7a0a
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2024-01-06 14:59:46 -0800

    Time: 129 ms (81.16%), Space: 76.8 MB (54.51%) - LeetHub

---
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int dp[50001];
+    vector<array<int, 3>> vp; //start, end, profit
+    vector<int> start, last, bonus;
+    int rec(int i) {
+        
+        if(i == size(start))
+            return 0;
+        
+        int &ret = dp[i];
+        if(~ret)
+            return ret;
+        
+        ret = rec(i + 1); //leave
+        
+        //try to take
+        int idx = lower_bound(begin(start), end(start), last[i]) - begin(start);
+        if(idx < size(start)) {
+            ret = max(ret, rec(idx) + bonus[i]);
+        }else {
+            ret = max(ret, rec(size(start)) + bonus[i]);
+        }
+        
+        return ret;
+    }
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        memset(dp, -1, sizeof dp);
+        for(int i = 0; i < size(startTime); i++) {
+            vp.push_back({startTime[i], endTime[i], profit[i]});
+        }
+        sort(begin(vp), end(vp));
+        for(int i = 0; i < size(startTime); i++) {
+            start.push_back(vp[i][0]);
+            last.push_back(vp[i][1]);
+            bonus.push_back(vp[i][2]);
+        }
+        return rec(0);
+    }
+};
