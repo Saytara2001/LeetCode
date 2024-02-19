d5d8ad3adbb386209b92e7406a5fe7211e5cf696
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2024-02-19 20:12:30 +0200

    Time: 9 ms (78.33%), Space: 9.4 MB (69.30%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,12 +5,11 @@ public:
         int l = 0 , r = 0, n = size(s);
         int mx = 0;
         while(l <= r and r < n) {
-            if(!freq[s[r]]) {
+            while(r < n and !freq[s[r]]) {
                 freq[s[r++]] = true;
-            }else {
-                 freq[s[l++]] = false;
             }
             mx = max(mx, r - l);
+            freq[s[l++]] = false;
         }
         return mx;
     }
