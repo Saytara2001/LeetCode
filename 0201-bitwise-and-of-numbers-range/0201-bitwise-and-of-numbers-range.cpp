66584c1d0030b040397c7ee7c278747615607b3b
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2024-02-21 14:04:34 +0200

    Time: 12 ms (33.74%), Space: 8.8 MB (26.31%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
-        //to make sure bit will be one in all numbers 
-        //must to be between two consecutive powers of 2's
-        //so if right i can subtract from him power of 2's then this bit will be 0
-        vector<bool> bit(33);
-        for(int i = 0; i <= 31; i++) {
-            bit[i] = (1LL << i) & right;
+        //get the common prefix
+        int shift = 0;
+        while(left != right) {
+            left >>= 1;
+            right >>= 1;
+            shift++;
         }
-        int j = 0;
-        for(int i = left; (1LL << j) + left <= right; i++) {
-            bit[j] = 0;
-            j++;
-        }
-        long long ans = 0;
-        for(int i = 0; i <= 32; i++) {
-            if(bit[i]) {
-                ans += (1LL << i);
-            }
-        }
-        ans &= (left & right);
-        return ans;
+        return (left << shift);
     }
 };
