da67adbd0e6d5860725f56f1825bf21bda191518
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2024-02-26 17:48:04 +0200

    Time: 98 ms (37.09%), Space: 123.6 MB (5.66%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
+        
         int n = size(nums);
-        vector<int> Mn(n), Mx(n);
         int mnNum = INT_MAX, mxNum = INT_MIN;
+        
+        vector<int> Mn(n), Mx(n);
+        
         for(int i = 0; i < n; i++) {
+            
             mnNum = min(mnNum, nums[i]);
             Mn[i] = mnNum;
+            
             mxNum = max(mxNum, nums[n - i - 1]);
             Mx[n - i - 1] = mxNum;
+            
         }
         for(int i = 0; i < n; i++) {
             if(nums[i] > Mn[i] and nums[i] < Mx[i])
