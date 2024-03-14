f1567a8d25938dbe6c01dd157c95d06f016aef13
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2024-03-14 11:34:33 +0200

    Time: 75 ms (16.71%), Space: 40.3 MB (10.17%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
         
-        map<int, int> mp;
+        map<int, int> mp{{0, 1}};
         int ans = 0;
         
-        mp[0]++;
-        
-        ans += mp[nums[0] - goal];
-        mp[nums[0]]++;
-        
-        for(int i = 1; i < size(nums); i++) {
+        for(int i = 1; i < size(nums); i++) 
             nums[i] += nums[i - 1];
+        
+        for(int i = 0;i < size(nums); i++) {
             ans += mp[nums[i] - goal];
             mp[nums[i]]++;
         }
+        
         return ans;
     }
 };
