b06be90210344efb9f7b8d926c3efb3c43e3bbf7
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2024-04-04 02:43:41 +0200

    Time: 1753 ms (5.01%), Space: 11.8 MB (18.36%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int n, m;
-    int dx[4] = {1,-1,0, 0};       
-    int dy[4] = {0, 0,1,-1,};
+    int dx[4] = {1, -1, 0,  0};       
+    int dy[4] = {0,  0, 1, -1,};
     vector<vector<bool>> vis;
-
     bool rec(int i, int j, int idx, string s, vector<vector<char>>& board, string& word) {
         if(idx == size(word)) 
             return true;
@@ -15,13 +14,9 @@ public:
             int ni = i + dx[d];
             int nj = j + dy[d];
             if(word[idx] == board[i][j]) {
-                
                 vis[i][j] = true;
-                
                 if(rec(ni, nj, idx + 1, s + board[i][j], board, word)) 
                     return true;
-                
-                
             }
         }
         vis[i][j] = false;
