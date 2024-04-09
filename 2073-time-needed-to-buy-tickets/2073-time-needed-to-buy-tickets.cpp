d25d0e476b0545167421b6352bcfebc4e17ababf
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2024-04-09 16:51:25 +0200

    Time: 4 ms (41.19%), Space: 9.4 MB (67.56%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -2,18 +2,14 @@ class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
         int n = size(tickets);
-        int front = (k + 1) * tickets[k];
-        int back = (n - k - 1) * (tickets[k] - 1);
-        int sum = front + back;
+        int time = 0;
         for(int i = 0; i < n; i++) {
             if(i <= k) {
-                if(tickets[i] - tickets[k] < 0) 
-                    sum += tickets[i] - tickets[k];
+                time += min(tickets[i], tickets[k]);
             }else {
-                if(tickets[i] - (tickets[k] - 1) < 0) 
-                    sum += tickets[i] - (tickets[k] - 1);
+                time += min(tickets[i], tickets[k] - 1);
             }
         }
-        return sum;
+        return time;
     }
 };
