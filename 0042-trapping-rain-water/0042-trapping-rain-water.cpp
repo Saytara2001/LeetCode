a9c7fa26b2215d2d307f06f117172552e15df767
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2024-04-15 14:17:10 +0200

    Time: 17 ms (12.05%), Space: 22.3 MB (82.34%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,30 +1,21 @@
-class Solution
-{
+class Solution { 
 public:
-    int trap(vector<int> &height)
-    {
-        int n = height.size(); // Number of elements in the input vector
-        int water = 0;     // Variable to store the total trapped water
-
-        // Arrays to store maximum height to the left and right of each index
-        vector<int> l_max(n), r_max(n);
-
-        // Calculate the maximum height to the left of each index
-        l_max[0] = height[0]; // First element's maximum height to the left is itself
-        for (int i = 1; i < n; i++)
-            l_max[i] = max(height[i], l_max[i - 1]);
-
-        // Calculate the maximum height to the right of each index
-        r_max[n - 1] = height[n - 1]; // Last element's maximum height to the right is itself
-        for (int i = n - 2; i >= 0; i--)
-            r_max[i] = max(height[i], r_max[i + 1]);
-
-        // Iterate through each index (excluding the first and last)
-        for (int i = 1; i < n - 1; i++)
-        {
-            // Calculate the maximum water current height can hold above itself
-            water += min(r_max[i], l_max[i]) - height[i];
+    int trap(vector<int> &height) {
+        int n = size(height);
+        int l = 0, r = n - 1;
+        int mxLeft = height[l], mxRight = height[r];
+        long long water = 0;
+        while(l < r) {
+            if(mxLeft < mxRight) {
+                l++;
+                mxLeft = max(mxLeft, height[l]);
+                water += mxLeft - height[l];
+            }else {
+                r--;
+                mxRight = max(mxRight, height[r]);
+                water += mxRight - height[r];
+            }
         }
-        return water; // Return the total trapped water
+        return water;
     }
 };
