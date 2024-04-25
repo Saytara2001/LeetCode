894de31b8cfe55736ada9eae3114fe42bbcfefb7
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2024-04-25 12:39:34 +0200

    Time: 272 ms (43.28%), Space: 31.5 MB (43.28%) - LeetHub

---
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int dp[100000][28];
+    int rec(int i, int prev, string& s, int k) {
+        
+        if(i == size(s)) return 0;
+        
+        int &ret = dp[i][prev];
+        if(~ret)
+            return ret;
+        
+        ret = 0;
+        ret = max(ret, rec(i + 1, prev, s, k)); //leave
+        if(prev == 27 or abs(prev - (s[i] - 'a')) <= k){
+            ret = max(ret, rec(i + 1, s[i] - 'a', s, k) + 1); //take
+        }
+        
+        return ret;
+    }
+    int longestIdealString(string s, int k) {
+        memset(dp, -1, sizeof dp);
+        return rec(0, 27, s, k);
+    }
+};
