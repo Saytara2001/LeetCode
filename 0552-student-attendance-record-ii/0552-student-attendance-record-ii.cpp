e832a162a34df67f1e0cea395b6105f4e39e83c0
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2024-05-26 12:43:39 +0300

    Time: 175 ms (65.84%), Space: 17.4 MB (81.99%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-    int dp[100001][2][2][3];
+    int dp[100001][2][3];
     int sz;
     int const mod = 1e9 + 7;
-    int rec(int i, int isLstL, int A, int L) {
+    int rec(int i, int A, int L) {
         if(L >= 3 or A >= 2) 
             return 0;
         
         if(i == sz) 
             return 1;
         
-        int &ret = dp[i][isLstL][A][L];
+        int &ret = dp[i][A][L];
         if(~ret)
             return ret;
         
-        ret = rec(i + 1, 0, A, L); //add P
-        ret += rec(i + 1, 0, A + 1, L); //add A
+        ret = rec(i + 1, A, 0); //add P
+        
+        ret += rec(i + 1, A + 1, 0); //add A
         ret %= mod;
-        if(isLstL) {
-            ret += rec(i + 1, 1, A, L + 1); //add L
-            ret %= mod;
-        }else {
-            ret += rec(i + 1, 1, A, 1); //add L
-            ret %= mod;
-        }
+        
+        ret += rec(i + 1, A, L + 1); //add L
+        ret %= mod;
+        
         return ret;
     }
     int checkRecord(int n) {
         sz = n;
         memset(dp, -1, sizeof dp);
-        return rec(0, 0, 0, 0);
+        return rec(0, 0, 0);
     }
 };
