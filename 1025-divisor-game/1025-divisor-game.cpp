75b4b3e90300b37e8d7d95d43747bdbeec201083
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2024-06-12 14:26:30 +0300

    Time: 2 ms (58.46%), Space: 7.1 MB (59.77%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
     bool divisorGame(int n) {
-        vector<bool> dp(n + 1, false); // 1 => win, 0 => lose
-        for(int i = 2; i <= n; i++) {
-            for(int x = 1; x * x < i; x++) {
-                if(i % x == 0) {
-                    if(dp[i - x] == false) { //lose
-                        dp[i] = true; //then can win here
-                        break;
-                    }
-                }
-            }
-        }
-        return dp[n];
+        return !(n & 1);
     }
 };
