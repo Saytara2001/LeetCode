c58ee56306439ca19b2335954058fca774c08429
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2024-06-12 15:44:58 +0300

    Time: 3 ms (23.31%), Space: 7.1 MB (23.83%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
+    int dp[1001][2];
+    int rec(int n, bool turn) {
+        
+        if(n == 1) return (turn == 0);
+        
+        int &ret = dp[n][turn];
+        if(~ret)
+            return ret;
+        
+        bool win = 0;
+        ret = 0;
+        for(int x = 1; x * x < n; x++) {
+            if(n % x == 0) {
+                if(turn == rec(n - x, !turn)) {
+                    win = 1;
+                    ret |= rec(n - x, !turn);
+                }
+                
+            }
+        }
+        if(!win) {
+            return ret = (turn == 0);
+        }
+        return ret;
+    }
     bool divisorGame(int n) {
-        return !(n & 1);
+        memset(dp, -1, sizeof dp);
+        return rec(n, 1);
     }
 };
