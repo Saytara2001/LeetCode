968699d3a292d5b8c67845cf1178d843b5cd1915
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2024-06-16 22:25:07 +0300

    Time: 3 ms (83.44%), Space: 13.7 MB (82.46%) - LeetHub

--- 0330-patching-array/0330-patching-array.cpp
+++ 0330-patching-array/0330-patching-array.cpp
@@ -2,21 +2,14 @@ class Solution {
 public:
     int minPatches(vector<int>& nums, int n) {
         long long sum = 0;
-        int cnt = 0;
-        for(int i = 0; i < size(nums); i++) {
-            if(sum + 1 >= nums[i]) {
-                sum += nums[i];
-            }else {
-                while(sum + 1 < nums[i] and sum < n) {
-                    sum += sum + 1;
-                    cnt++;
-                }
-                sum += nums[i];
-            }
-        }
+        int cnt = 0, i = 0, sz = size(nums);
         while(sum < n) {
-            sum += sum + 1;
-            cnt++;
+            if(i < sz and sum + 1 >= nums[i]) {
+                sum += nums[i++];
+            }else {
+                sum += sum + 1;
+                cnt++;
+            }
         }
         return cnt;
     }
