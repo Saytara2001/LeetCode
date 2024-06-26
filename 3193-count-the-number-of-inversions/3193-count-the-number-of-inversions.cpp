5b78dd5ecf87d163b7069c5ae2cacbc59e7bbfc8
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2024-06-26 18:02:30 +0300

    Time: 1047 ms (8.30%), Space: 47.7 MB (58.55%) - LeetHub

--- 3193-count-the-number-of-inversions/3193-count-the-number-of-inversions.cpp
+++ 3193-count-the-number-of-inversions/3193-count-the-number-of-inversions.cpp
@@ -3,12 +3,12 @@ public:
     const int mod = 1e9 + 7;
     int numberOfPermutations(int n, vector<vector<int>>& requirements) {
         int k = 401;
-        unordered_map<int, int> mp;
+        map<int, int> mp;
         for(auto it: requirements) {
             mp[it[0] + 1] = it[1];
         }
         vector<vector<int>> dp(n + 1, vector<int>(k + 1));
-        dp[0][0] = 1;
+        dp[1][0] = 1;
         for (int i = 1; i <= n; ++i) { // permutation of length => i
             for (int j = 0; j <= k; ++j) { // inversion
                 for (int l = 0; l < i; ++l) {
