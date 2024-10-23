666b63644ca1ba715e51386b23635c67ac761e73
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2024-10-23 17:57:58 +0300

    Time: 4 ms (99.10%), Space: 30.2 MB (76.35%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
-    int dp[40001][3];
-    int rec(int i, int sum, vector<int>& v) {
-        if(i == size(v))
-            return sum == 0 ? 0 : -1e9;
-        
-        int &ret = dp[i][sum];
-        if(~ret)
-            return ret;
-        
-        ret = -1e9;
-        ret = max(ret, rec(i + 1, sum, v)); // leave
-        ret = max(ret, v[i] + rec(i + 1, (sum + v[i]) % 3, v)); // take
-        
-        
-        return ret;
-    }
     int maxSumDivThree(vector<int>& nums) {
-        memset(dp, -1, sizeof dp);
-        return rec(0, 0, nums);
+        int sum = 0;
+        vector<int> rem1, rem2;
+        for(auto num: nums) {
+            sum += num;
+            if(num % 3 == 1) rem1.push_back(num);   
+            if(num % 3 == 2) rem2.push_back(num);
+        }
+        
+        sort(begin(rem1), end(rem1));
+        sort(begin(rem2), end(rem2));
+        
+        int cur = 0;
+        if(sum % 3 == 1) {
+            // remove 1 element from rem1 or 2 elements from rem2
+            if(rem1.size())
+                cur = sum - rem1[0];
+            
+            if(rem2.size() > 1)
+                cur = max(cur, sum - (rem2[0] + rem2[1]));
+            sum = cur;
+        }else if(sum % 3 == 2) {
+            
+            // remove 2 elements from rem1 or 1 element from rem2
+            if(rem1.size() > 1)
+                cur = sum - rem1[0] - rem1[1];
+            
+            if(rem2.size())
+                cur = max(cur, sum - rem2[0]);
+            
+            sum = cur;
+        }
+        
+        return sum;    
     }
 };
