5acca5a94ae37f13070fe77f9bb7bae370bf94d1
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2024-11-15 10:15:48 +0200

    Time: 0 ms (100.00%), Space: 69.6 MB (66.98%) - LeetHub

--- 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
+++ 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
@@ -1,35 +1,20 @@
 class Solution {
 public:
     int findLengthOfShortestSubarray(vector<int>& arr) {
-        arr.insert(begin(arr), 0); // dummy value
-        arr.push_back(1e9); // dummy value
-        
         int n = size(arr);
-        vector<int> inc(n);
         
-        inc[n - 1] = 1;
-        for(int i = n - 2; i >= 0; i--) {
-            if(arr[i] <= arr[i + 1]) {
-                inc[i] = 1;
-            }else 
-                break;
-        }
-        int shortest = n;
-        for(int i = 0; i < n; i++) {
-            
-            if(i > 0 and arr[i] < arr[i - 1]) 
-                break;
-            
-            int l = i, r = n;
-            while(l < r) {
-                int mid = l + (r - l) / 2;
-                if(inc[mid] and arr[i] <= arr[mid]) {
-                    r = mid;
-                }else {
-                    l = mid + 1;
-                }
+        int r = n - 1;
+        while(r > 0 and arr[r - 1] <= arr[r]) 
+            --r;
+        
+        int l = 0, shortest = r;
+        while(l < r and (l == 0 or arr[l - 1] <= arr[l])) {
+            // Find the next valid number
+            while(r < n and arr[l] > arr[r]) {
+                ++r;
             }
-            shortest = min(shortest, max(n - (i + 1) - (n - r), 0));
+            shortest = min(shortest, r - l - 1);
+            ++l;
         }
         return shortest;
     }
