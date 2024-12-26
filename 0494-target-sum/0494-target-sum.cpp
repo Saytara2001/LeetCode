d74ac00c7a55ba8da9b7247719c766be1600927a
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2024-12-26 16:01:52 +0200

    Time: 12 ms (62.53%), Space: 24.5 MB (31.27%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -5,19 +5,21 @@ public:
         int n = size(nums);
         int totalSum = accumulate(begin(nums), end(nums), 0);
         
-        vector<vector<int>> dp(n, vector<int>(2 * totalSum + 1));
-        dp[0][nums[0] + totalSum] = 1;
-        dp[0][-nums[0] + totalSum] += 1;
+        vector<int> dp(2 * totalSum + 1);
+        dp[nums[0] + totalSum] = 1;
+        dp[-nums[0] + totalSum] += 1;
         
         for(int i = 1; i < n; i++) {
+            vector<int> next(2 * totalSum + 1);
             for(int sum = -totalSum; sum <= totalSum; sum++) {
-                if(dp[i - 1][sum + totalSum] > 0) {
-                    dp[i][sum + nums[i] + totalSum] += dp[i - 1][sum + totalSum];
-                    dp[i][sum - nums[i] + totalSum] += dp[i - 1][sum + totalSum];
+                if(dp[sum + totalSum] > 0) {
+                    next[sum + nums[i] + totalSum] += dp[sum + totalSum];
+                    next[sum - nums[i] + totalSum] += dp[sum + totalSum];
                 }
             }
+            dp = next;
         }
         
-        return abs(target) > totalSum ? 0 : dp[n - 1][target + totalSum];
+        return abs(target) > totalSum ? 0 : dp[target + totalSum];
     }
 };
