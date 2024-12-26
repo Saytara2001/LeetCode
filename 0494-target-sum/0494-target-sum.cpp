bd364abcc0303aa58568949c3d0844f23b1710a2
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2024-12-26 14:17:08 +0200

    Time: 15 ms (59.97%), Space: 15 MB (47.55%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
+    int dp[20][40005];
     int rec(int i, int sum, int target, vector<int>& nums) {
         
         if(i == size(nums)) {
             return sum == target;
         }
         
-        int ways = rec(i + 1, sum + nums[i], target, nums);
-        ways += rec(i + 1, sum - nums[i], target, nums);
+        int fixSum = sum < 0 ? 20000 + -sum : sum;
+        int &ret = dp[i][fixSum];
+        if(~ret)
+            return ret;
         
-        return ways;
+        ret = rec(i + 1, sum + nums[i], target, nums);
+        ret += rec(i + 1, sum - nums[i], target, nums);
+        
+        return ret;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
+        memset(dp, -1, sizeof dp);
         return rec(0, 0, target, nums);
     }
 };
