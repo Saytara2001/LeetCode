7256fba363ef74e10cb029c694cbd80ff814fc85
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2024-12-29 16:57:39 +0200

    Time: 0 ms (100.00%) | Memory: 16.2 MB (51.90%) - LeetSync

--- 779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.cpp
+++ 779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
-        multiset<int> ms1, ms2;
+        
+        int n = size(arr), cuts = 0;
         vector<int> sortedArray = arr;
         sort(begin(sortedArray), end(sortedArray));
-        int n = size(arr), cuts = 0;;
+        
+        long long s1 = 0, s2 = 0;
         for(int i = 0; i < n; i++) {
-            ms1.insert(sortedArray[i]);
-            ms2.insert(arr[i]);
-            cuts += (ms1 == ms2);
+            s1 += sortedArray[i];
+            s2 += arr[i];
+            cuts += (s1 == s2);
         }
         return cuts;
     }
