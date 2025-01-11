90a77f0f80b6cde42684a2ca6a848127f6f76cab
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2025-01-11 20:58:28 +0200

    Time: 23 ms (5.22%) | Memory: 71.7 MB (41.98%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,14 +1,39 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int n = size(nums);
-        int sum = 0, maxSum = -2e9, minSum = 0;
-        // using prefix Sum
-        for(int i = 0; i < n; i++) {
-            sum += nums[i];
-            maxSum = max(maxSum, sum - minSum);
-            minSum = min(minSum, sum);
+    int MaxCrossingSum(int l, int mid, int r, vector<int> &v) {
+        int maxLeftSum = INT_MIN, maxRightSum = INT_MIN, sum = 0;
+        for (int i = mid; i >= l; --i) {
+            sum += v[i];
+            maxLeftSum = max(maxLeftSum, sum);
         }
-        return maxSum;
+        sum = 0;
+        for (int i = mid + 1; i <= r; ++i) {
+            sum += v[i];
+            maxRightSum = max(maxRightSum, sum);
+        }
+        return maxLeftSum + maxRightSum;
+    }
+
+    int MaxSum(int l, int r, vector<int> &v) {
+        if (l > r)
+            return INT_MIN;
+        if (l == r)
+            return v[l];
+
+        int mid = l + (r - l) / 2;
+
+        int leftSum = MaxSum(l, mid, v);
+        int rightSum = MaxSum(mid + 1, r, v);
+        int crossSum = MaxCrossingSum(l, mid, r, v);
+
+        if (leftSum >= rightSum and leftSum >= crossSum)
+            return leftSum;
+        else if (rightSum >= leftSum and rightSum >= crossSum)
+            return rightSum;
+        else
+            return crossSum;
+    }
+    int maxSubArray(vector<int>& nums) {
+        return MaxSum(0, size(nums) - 1, nums);
     }
 };
