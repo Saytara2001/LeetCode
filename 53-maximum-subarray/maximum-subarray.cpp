484dc4a3e3d78f4d13f9d30e50f6bf29cacd0d96
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2025-01-11 20:14:52 +0200

    Time: 1 ms (27.36%) | Memory: 71.7 MB (41.98%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int n = size(nums);
-        int sum = 0, maxSum = -2e9;
-        // using kadane algorthim
+        int sum = 0, maxSum = -2e9, minSum = 0;
+        // using prefix Sum
         for(int i = 0; i < n; i++) {
             sum += nums[i];
-            maxSum = max(maxSum, sum);
-            if(sum < 0) sum = 0;
+            maxSum = max(maxSum, sum - minSum);
+            minSum = min(minSum, sum);
         }
         return maxSum;
     }
