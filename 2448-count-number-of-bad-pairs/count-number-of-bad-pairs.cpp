a38683d4a1726fc4cbd12d0ea8943f6e2911b148
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2025-02-09 16:04:03 +0200

    Time: 111 ms (15.04%) | Memory: 89.3 MB (39.21%) - LeetSync

---
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long countBadPairs(vector<int>& nums) {
+
+        int n = size(nums);
+        long long res = 1LL * n * (n - 1) / 2;
+
+        map<int, int> mp;
+        for(int i = 0; i < n; i++) {
+            res -= mp[nums[i] - (i + 1)]++;
+        }
+
+        return res;
+    }
+};
