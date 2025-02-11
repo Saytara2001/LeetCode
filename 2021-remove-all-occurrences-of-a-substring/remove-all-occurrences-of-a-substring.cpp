defcc032cee9801f86dadc463111ae0304953aaf
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2025-02-11 12:22:18 +0200

    Time: 0 ms (100.00%) | Memory: 11.1 MB (10.28%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,17 +1,49 @@
 class Solution {
 public:
-    string removeOccurrences(string s, string part) {
-        string res = "";
-        int m = part.size();
-        for(auto c: s) {
-            res += c;
-            int n = res.size();
-            if(n >= m and res.substr(n - m, m) == part) {
-                for(int cnt = 0; cnt < m; cnt++) 
-                    res.pop_back();
-            } 
-        }
+    vector<int> computePrefix(string &pat) {
+        int n = pat.size();
+        vector<int> longestPrefix(n);
+        for (int i = 1, k = 0; i < n; ++i) {
+            while (k > 0 and pat[i] != pat[k])
+                k = longestPrefix[k - 1];
 
-        return res;
+            if (pat[i] == pat[k])
+                longestPrefix[i] = ++k;
+            else
+                longestPrefix[i] = k;
+        }
+        return longestPrefix;
+    }
+
+    string Kmp(string &s, string &pat) {
+
+        int n = s.size();
+        int m = pat.size();
+        vector<int> longestPrefix = computePrefix(pat);
+
+        int i = 0, k = 0;
+        while (i < n) {
+
+            if (s[i] == pat[k]) {
+                ++k, ++i;
+            } else {
+                if (k > 0)
+                    k = longestPrefix[k - 1]; // fail to try match
+                else
+                    ++i; // we can't match 
+            }
+
+            // matched pattern
+            if (k == m) {
+                s = s.substr(0, i - m) + s.substr(i, n);
+                i = max(i - 2 * m, 0); // match.push_back(i - m + 1);
+                k = 0; // start to match pattern from first again
+                n = size(s);
+            }
+        }
+        return s;
+    }
+    string removeOccurrences(string s, string part) {
+        return Kmp(s, part);
     }
 };
