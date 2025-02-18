407b91ceefdeaa104330539778d046d5c2dad921
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2025-02-18 09:01:24 +0200

    Time: 12 ms (25.36%) | Memory: 7.7 MB (85.60%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int best;
-    bool rec(int i, int lst, int gen, long msk, string& pat) {
+    
+    int rec(int i, int lst, int gen, long msk, string& pat) {
+        int best = INT_MAX;
         if(i == size(pat) + 1) {
-            best = min(gen, best);
-            return true;
+            return gen;
         }
 
         for(int x = 1; x <= 9; x++) {
             if(msk & (1 << (x - 1))) continue;
             if(i > 0 and pat[i - 1] == 'I' and x < lst) continue;
             if(i > 0 and pat[i - 1] == 'D' and x > lst) continue;
-            rec(i + 1, x, gen * 10 + x, msk | (1 << (x - 1)), pat);
+            best = min(best, rec(i + 1, x, gen * 10 + x, msk | (1 << (x - 1)), pat));
         }
 
-        return false;
+        return best;
     }
     string smallestNumber(string pattern) {
-        best = INT_MAX;
         int res = rec(0, 1, 0, 0, pattern);
-        string ans = to_string(best);
+        string ans = to_string(res);
         return ans;
     }
 };
