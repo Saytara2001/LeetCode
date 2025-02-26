504b02a1ed77f113b41a4f906617b78e5689bc35
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2025-02-26 07:57:17 +0200

    Time: 0 ms (100.00%) | Memory: 45.2 MB (51.09%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -4,19 +4,13 @@ public:
         int n = size(nums);
         int maxSum = 0, cur = 0;
         for(int i = 0; i < n; i++) {
-            cur += nums[i];
-            if(cur < 0) {
-                cur = 0;
-            }
+            cur = max(cur + nums[i], nums[i]);
             maxSum = max(maxSum, cur);
             nums[i] = -nums[i];
         }
         cur = 0;
         for(int i = 0; i < n; i++) {
-            cur += nums[i];
-            if(cur < 0) {
-                cur = 0;
-            }
+            cur = max(cur + nums[i], nums[i]);
             maxSum = max(maxSum, cur);
         }
         return maxSum;
