eeb59d38a448d43cd1ff5c9976b07529a9da7704
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2025-02-26 13:50:29 +0200

    Time: 762 ms (8.96%) | Memory: 280.4 MB (5.32%) - LeetSync

--- 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.cpp
+++ 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.cpp
@@ -6,27 +6,21 @@ public:
         int n = size(matrix), m = size(matrix[0]);
         vector<vector<int>> pref(n + 1, vector<int>(m + 1));
 
+        int sub = 0;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 pref[i + 1][j + 1] = pref[i][j + 1] + pref[i + 1][j] - pref[i][j] + matrix[i][j];
             }
         }
-
-        int sub = 0;
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= m; j++) {
-
-                for(int x = 1; x <= i; x++) {
-                    for(int y = 1; y <= j; y++) {
-
-                        int val = pref[i][j];
-                        val -= pref[x - 1][j] + pref[i][y - 1];
-                        val += pref[x - 1][y - 1];
-                        if(val == target) {
-                            ++sub;
-                        }
-
-                    }
+        
+        for(int r2 = 1; r2 <= n; r2++) {
+            for(int r1 = 0; r1 < r2; r1++) {
+                unordered_map<int, int> mp;
+                mp[0] = 1;
+                for(int c = 1; c <= m; c++) {
+                    int val = pref[r2][c] - pref[r1][c];
+                   sub += mp[val - target];
+                   ++mp[val];
                 }
             }
         }
