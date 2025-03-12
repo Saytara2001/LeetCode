ee8ec01cec566584bbb65c76e5dc397bc26e0ca8
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2025-03-12 17:08:41 +0200

    Time: 55 ms (52.14%) | Memory: 66.2 MB (22.98%) - LeetSync

--- 2409-number-of-increasing-paths-in-a-grid/number-of-increasing-paths-in-a-grid.cpp
+++ 2409-number-of-increasing-paths-in-a-grid/number-of-increasing-paths-in-a-grid.cpp
@@ -1,48 +1,43 @@
 class Solution {
 public:
-    int countPaths(vector<vector<int>>& grid) {
-        int n = size(grid), m = size(grid[0]);
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-
-        const int mod = 1e9 + 7;
-        int dx[4] = {0, 0, 1, -1};
-        int dy[4] = {1, -1, 0, 0};
-
-        // Lambda to add two numbers with modulo
-        auto add = [&](int a, int b) {
-            return (a % mod + b % mod) % mod;
-        };
-
-        // Lambda to check if a cell is valid
-        auto valid = [&](int i, int j) {
-            return (i >= 0 and i < n and j >= 0 and j < m);
-        };
-
-        int paths = n * m; // Paths with one length (one cell)
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                paths = add(paths, rec(i, j, dp, grid, dx, dy, add, valid));
-            }
-        }
-
-        return paths;
+    int n, m;
+    const int mod = 1e9 + 7;
+    int dx[4] = {0, 0, 1, -1};
+    int dy[4] = {1, -1, 0, 0};
+    int add(int a, int b) {
+        return (a % mod + b % mod) % mod;
     }
+    bool valid(int& i, int& j, int& n, int& m) {
+        return (i >= 0 and i < n and j >= 0 and j < m);
+    }
+    int rec(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp) {
 
-private:
-    int rec(int i, int j, vector<vector<int>>& dp, vector<vector<int>>& grid, const int dx[4], const int dy[4], function<int(int, int)> add, function<bool(int, int)> valid) {
-        int &ret = dp[i][j];
-        if (~ret)
+            int &ret = dp[i][j];
+            if(~ret)
+                return ret;
+
+            ret = 1;
+            for(int d = 0; d < 4; d++) {
+                int ni = i + dx[d];
+                int nj = j + dy[d];
+                if(valid(ni, nj, n, m) and grid[i][j] < grid[ni][nj]) {
+                    ret = add(ret, rec(ni, nj, grid, dp));
+                }
+            }
+
             return ret;
+    };
+    int countPaths(vector<vector<int>>& grid) {
 
-        ret = 0;
-        for (int d = 0; d < 4; d++) {
-            int ni = i + dx[d];
-            int nj = j + dy[d];
-            if (valid(ni, nj) and grid[i][j] < grid[ni][nj]) {
-                ret = add(ret, rec(ni, nj, dp, grid, dx, dy, add, valid) + 1);
+        n = size(grid), m = size(grid[0]);
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        
+        int paths = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                paths = add(paths, rec(i, j, grid, dp));
             }
         }
-
-        return ret;
+        return paths;
     }
 };
