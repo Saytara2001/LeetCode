db96f8102bb1ead91ccd008607c05b650deb9e88
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2025-03-12 16:37:33 +0200

    Time: 1655 ms (5.15%) | Memory: 85.8 MB (16.30%) - LeetSync

--- 2409-number-of-increasing-paths-in-a-grid/number-of-increasing-paths-in-a-grid.cpp
+++ 2409-number-of-increasing-paths-in-a-grid/number-of-increasing-paths-in-a-grid.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
+    const int mod = 1e9 + 7;
+    int dx[4] = {0, 0, 1, -1};
+    int dy[4] = {1, -1, 0, 0};
+    int add(int a, int b) {
+        return (a % mod + b % mod) % mod;
+    }
+    bool valid(int& i, int& j, int& n, int& m) {
+        return (i >= 0 and i < n and j >= 0 and j < m);
+    }
     int countPaths(vector<vector<int>>& grid) {
 
         int n = size(grid), m = size(grid[0]);
         vector<vector<int>> dp(n, vector<int>(m, -1));
-
-        const int mod = 1e9 + 7;
-        int dx[4] = {0, 0, 1, -1};
-        int dy[4] = {1, -1, 0, 0};
-        auto add = [&](int a, int b) {
-            return (a % mod + b % mod) % mod;
-        };
-        auto valid = [&](int i, int j) {
-            return (i >= 0 and i < n and j >= 0 and j < m);
-        };
 
         function<int(int, int)> rec = [&](int i, int j) {
 
@@ -25,7 +24,7 @@ public:
             for(int d = 0; d < 4; d++) {
                 int ni = i + dx[d];
                 int nj = j + dy[d];
-                if(valid(ni, nj) and grid[i][j] < grid[ni][nj]) {
+                if(valid(ni, nj, n, m) and grid[i][j] < grid[ni][nj]) {
                     ret = add(ret, rec(ni, nj) + 1);
                 }
             }
