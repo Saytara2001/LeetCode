fd813db7f3aa2fb978429c6ff7d92894c7f6f3f5
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2025-03-24 15:05:22 +0200

    Time: 68 ms (64.63%) | Memory: 126.6 MB (43.20%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/count-days-without-meetings.cpp
@@ -3,22 +3,15 @@ public:
     int countDays(int days, vector<vector<int>>& meetings) {
         int n = size(meetings);
         sort(begin(meetings), end(meetings));
-        vector<array<int, 2>> res;
+        int lstEnd = 0, freeDays = 0;
         for(int i = 0; i < n; i++) {
-            if(res.size() and res.back()[1] >= meetings[i][0]) {
-                int mn = min(res.back()[0], meetings[i][0]);
-                int mx = max(res.back()[1], meetings[i][1]);
-                res.pop_back();
-                res.push_back({mn, mx});
-            } else {
-                res.push_back({meetings[i][0], meetings[i][1]});
-            }
+            int st = meetings[i][0], end = meetings[i][1];
+            if(st > lstEnd) {
+                freeDays += st - lstEnd - 1;
+            } 
+            lstEnd = max(lstEnd, end);
         }
-        int cnt = res[0][0] - 1; // get first days before work
-        for(int i = 1; i < size(res); i++) {
-            cnt += res[i][0] - res[i - 1][1] - 1;
-        }
-        cnt += days - res.back()[1]; // get last days after work
-        return cnt;
+        freeDays += days - lstEnd;
+        return freeDays;
     }
 };
