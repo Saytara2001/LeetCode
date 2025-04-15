85bd77af401daf9f6a7661d7ddb227db63ef43ed
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2025-04-15 16:44:38 +0200

    Time: 27 ms (79.39%) | Memory: 92.7 MB (60.00%) - LeetSync

--- 2280-count-good-triplets-in-an-array/count-good-triplets-in-an-array.cpp
+++ 2280-count-good-triplets-in-an-array/count-good-triplets-in-an-array.cpp
@@ -1,48 +1,56 @@
-class FenwickTree {
-private:
-    vector<int> tree;
-
-public:
-    FenwickTree(int size) : tree(size + 1, 0) {}
-
-    void update(int index, int delta) {
-        index++;
-        while (index < tree.size()) {
-            tree[index] += delta;
-            index += index & -index;
+struct FenwickTree {
+    int N;
+    vector<int> bit;
+    void init(int n) {
+        N = n + 5;
+        bit.resize(N);
+    }
+    void add(int idx, int val) {
+        while (idx < N) {
+            bit[idx] += val; //change here
+            idx += idx & -idx;
         }
     }
 
-    int query(int index) {
-        index++;
-        int res = 0;
-        while (index > 0) {
-            res += tree[index];
-            index -= index & -index;
+    int query(int idx) {
+        int ret = 0;
+        while (idx > 0) {
+            ret += bit[idx]; //change here
+            idx -= idx & -idx;
         }
-        return res;
+        return ret;
     }
 };
 
 class Solution {
 public:
     long long goodTriplets(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        vector<int> pos2(n), reversedIndexMapping(n);
-        for (int i = 0; i < n; i++) {
-            pos2[nums2[i]] = i;
+        int n = size(nums1);
+        FenwickTree treeL, treeR;
+        treeL.init(n);
+        treeR.init(n);
+        vector<int> pos(n), reversedIndexMap(n);
+        for (int i = 0; i < n; ++i) {
+            pos[nums2[i]] = i;
         }
-        for (int i = 0; i < n; i++) {
-            reversedIndexMapping[pos2[nums1[i]]] = i;
+        for (int i = 0; i < n; ++i) {
+            reversedIndexMap[pos[nums1[i]]] = i; // give the index of nums2 in nums1
         }
-        FenwickTree tree(n);
+        vector<int> left(n), right(n);
+        for (int i = 0; i < n; ++i) { // loop on values in nums2
+            int idx = reversedIndexMap[i] + 1; // give me the index of nums2 in nums1
+            left[i] = treeL.query(idx - 1);
+            treeL.add(idx, 1);
+        }
+        for (int i = n - 1; i >= 0; --i) { // loop on values in nums2
+            int idx = reversedIndexMap[i] + 1; // give me the index of nums2 in nums1
+            int total = treeR.query(n);
+            right[i] = total - treeR.query(idx - 1);
+            treeR.add(idx, 1);
+        }
         long long res = 0;
-        for (int value = 0; value < n; value++) {
-            int pos = reversedIndexMapping[value];
-            int left = tree.query(pos);
-            tree.update(pos, 1);
-            int right = (n - 1 - pos) - (value - left);
-            res += (long long)left * right;
+        for (int i = 0; i < n; ++i) {
+            res += 1LL * left[i] * right[i];
         }
         return res;
     }
