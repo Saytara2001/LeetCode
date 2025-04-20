1df2180632b7ebad5a44d723ddad0282a235ea6f
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2025-04-20 20:50:18 +0200

    Time: 0 ms (100.00%) | Memory: 12.1 MB (71.00%) - LeetSync

---
+++ 797-rabbits-in-forest/rabbits-in-forest.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numRabbits(vector<int>& answers) {
+        unordered_map<int, int> mp;
+        for(auto x: answers) {
+            ++mp[x];
+        }
+        int n = 0;
+        for(auto [x, freq]: mp) {
+            if(x == 0) {
+                n += freq;
+                continue;
+            }
+             // freq / x + 1 with ceiling
+            int calc = (freq + x) / (x + 1);
+            n += max(calc * (x + 1), x + 1);
+        }
+        return n;
+    }
+};
