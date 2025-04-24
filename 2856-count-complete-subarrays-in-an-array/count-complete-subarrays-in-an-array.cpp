7e424f6d2e7ce7fc37f09f4e63cf3965b7ca6f29
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2025-04-24 08:39:07 +0200

    Time: 5 ms (98.26%) | Memory: 47.5 MB (26.91%) - LeetSync

---
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int countCompleteSubarrays(vector<int>& nums) {
+        int uni = 0, n = nums.size();
+        vector<int> freq(2001);
+        for(int i = 0; i < n; i++) {
+            if(!freq[nums[i]]) {
+                ++uni;
+                freq[nums[i]]++;
+            }
+        }
+        freq.assign(2001, 0);
+        int cnt = 0;
+        int l = 0, r = 0, dis = 0;
+        while(l < n) {
+            // move r
+            while(r < n and dis < uni) {
+                if(!freq[nums[r]]) {
+                    ++dis;
+                }
+                freq[nums[r]]++;
+                r++;
+            }
+            // check validality
+            if(dis == uni) 
+                cnt += n - r + 1;
+            // move l  
+            if(--freq[nums[l]] == 0) {
+                --dis;
+            }
+            ++l;
+        }
+        return cnt;
+    }
+};
