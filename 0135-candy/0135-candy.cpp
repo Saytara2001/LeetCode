0f28ca2097f9c6542b459688471135ed74217cb8
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2025-06-02 05:08:43 +0300

    Time: 2 ms (31.26%), Space: 22.9 MB (59.49%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,32 +1,24 @@
-// Define a class named Solution.
 class Solution {
 public:
-    // Define a method named candy.
-    int candy(vector<int>& ratings) {
-        int n = ratings.size(); // Get the number of children.
-        vector<int> left(n, 1), right(n, 1); // Create vectors to store left and right candy counts for each child.
-
-        // Calculate the left candies.
-        for (int i = 1; i < n; ++i) {
-            if (ratings[i] > ratings[i - 1]) {
-                left[i] = left[i - 1] + 1;
+    int candy(vector<int>& v) {
+        int n = size(v);
+        vector<int> candy(n, 1);
+        for(int i = 0; i < n; i++) {
+            //check if child local minimum
+            if((i - 1 < 0 or v[i] <= v[i - 1]) and (i + 1 >= n or v[i] <= v[i + 1])) {
+                candy[i] = 1;
+            }else if(i > 0 and v[i - 1] < v[i]) {
+                candy[i] = candy[i - 1] + 1;
+            }
+        }
+        
+        for(int i = n - 2; i >= 0; i--) {
+            if(v[i] > v[i + 1]) {
+                candy[i] = max(candy[i], candy[i + 1] + 1);
             }
         }
 
-        // Calculate the right candies.
-        for (int i = n - 2; i >= 0; --i) {
-            if (ratings[i] > ratings[i + 1]) {
-                right[i] = right[i + 1] + 1;
-            }
-        }
-
-        int totalCandies = 0; // Initialize a variable to store the total number of candies given.
-
-        // Calculate the maximum candies for each child from the left and right counts and sum them up.
-        for (int i = 0; i < n; ++i) {
-            totalCandies += max(left[i], right[i]);
-        }
-
-        return totalCandies; // Return the total number of candies given to all children.
+        int candies = accumulate(begin(candy), end(candy), 0);
+        return candies;
     }
 };
