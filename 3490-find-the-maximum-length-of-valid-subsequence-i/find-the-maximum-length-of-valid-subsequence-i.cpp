21f3343206a038b9e5f7e30302d5ef5936dd1264
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2025-07-17 23:22:17 +0300

    Time: 5 ms (50.96%) | Memory: 96.1 MB (60.77%) - LeetSync

--- 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int maximumLength(vector<int>& nums) {
-        vector<vector<int>> v(2);
         int n = size(nums);
+        vector<vector<int>> v(2);
+        int even = 0, odd = 0, alt = 1, lst = -1;
         for(int i = 0; i < n; i++) {
-            v[nums[i] % 2].push_back(i);
-        }
-        int maxLen = max(v[0].size(), v[1].size());
-        int i = 0, cnt = 1;
-        while(true) {
-            int need = 1 - (nums[i] % 2); // swap (0, 1)
-            auto it = upper_bound(begin(v[need]), end(v[need]), i);
-            if(it != end(v[need])) {
-                i = *it;
-                ++cnt;
-            } else {
-                break;
+            if(nums[i] & 1) ++odd;
+            if(!(nums[i] & 1)) ++even;
+
+            if(lst == -1) 
+                lst = nums[i];
+            else if(lst % 2 != nums[i] % 2) {
+                lst = nums[i];
+                ++alt;
             }
         }
-        maxLen = max(maxLen, cnt);
-        return maxLen;
+        return max({even, odd, alt});
     }
 };
