5fa6437fb721409fae898ac123c95f03baae7068
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2025-08-02 23:22:42 +0300

    Time: 95 ms (22.38%) | Memory: 108.3 MB (13.29%) - LeetSync

--- 2689-rearranging-fruits/rearranging-fruits.cpp
+++ 2689-rearranging-fruits/rearranging-fruits.cpp
@@ -2,29 +2,38 @@ class Solution {
 public:
     long long minCost(vector<int>& basket1, vector<int>& basket2) {
         int min_all = INT_MAX;
-        unordered_map<int, int> freq;
+        unordered_map<int, int> freq1, freq2, all;
         for(auto x: basket1) {
-            ++freq[x];
+            ++freq1[x];
+            ++all[x];
             min_all = min(min_all, x);
         }
         for(auto y: basket2) {
-            --freq[y];
+            ++freq2[y];
+            ++all[y];
             min_all = min(min_all, y);
         }
         vector<int> rem; // must be even
-        for(auto [a, f]: freq) {
-            if(f % 2 != 0)
+        for(auto [a, f]: all) {
+            if(f & 1)
                 return -1;
-            for(int i = 0; i < abs(f) / 2; i++)
+            if(freq1[a] == freq2[a]) continue;
+            int diff = abs(freq1[a] - freq2[a]);
+            for(int i = 0; i < diff / 2; i++)
                 rem.push_back(a);
         }
         int sz = rem.size();
+        if(sz == 0) 
+            return 0;
         sort(rem.begin(), rem.end());
-        long long ans = 0;
+        int min_val = rem.front();
+        long long ans = 1LL * min_val * (sz - 1);
+        long long ans2 = 0;
         for(int i = 0; i < sz / 2; i++) {
             int a = rem[i];
-            ans += min(a, min_all * 2);
+            int b = rem[sz - i - 1];
+            ans2 += min(a, min_all * 2);
         }
-        return ans;
+        return min(ans, ans2);
     }
 };
