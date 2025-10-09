577c357c45cc6ab2d9652eeaa7d5aa5ae2ebcd19
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2025-10-09 14:49:27 +0300

    Time: 125 ms (5.61%) | Memory: 99.4 MB (36.87%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    long long rec(int i, int lstOp, int &fee, vector<int>& prices, vector<vector<long long>> &dp) {
+    int rec(int i, int lstOp, int &fee, vector<int>& prices, vector<vector<int>> &dp) {
         if (i == size(prices)) return 0;
 
-        long long &ret = dp[i][lstOp];
-        if (ret != -1e18)
+        int &ret = dp[i][lstOp];
+        if (ret != -2e9)
             return ret;
 
         ret = max(ret, rec(i + 1, lstOp, fee, prices, dp));
 
         if (lstOp == 0) {
-            ret = max(ret, rec(i + 1, 1, fee, prices, dp) - prices[i]);
+            ret = max(ret, rec(i + 1, 1, fee, prices, dp) - prices[i] - fee);
         } else if (lstOp == 1){
-            ret = max(ret, rec(i + 1, 0, fee, prices, dp) + prices[i] - fee);
+            ret = max(ret, rec(i + 1, 0, fee, prices, dp) + prices[i] );
         }
         
         return ret;
     }
     int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
-        vector<vector<long long>> dp(n, vector<long long>(2, -1e18));
+        vector<vector<int>> dp(n, vector<int>(2, -2e9));
         return rec(0, 0, fee, prices, dp);
     }
 };
