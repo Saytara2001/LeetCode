220287ba082471baf4029a52555219a48cedac16
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2025-10-16 09:18:14 +0300

    Time: 3057 ms (45.74%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,5 +1,4 @@
 /* Write your T-SQL query statement below */
-
-select P.product_name, S.year, S.price
+select P.product_name , S.year , S.price 
 from Sales S join Product P
-on S.product_id = P.product_id 
+on S.product_id = P.product_id
