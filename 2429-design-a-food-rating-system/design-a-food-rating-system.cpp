0c77b665ffc73f7a7f3d63bc3ffcd2d01e77c7fa
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2025-10-23 20:33:06 +0300

    Time: 142 ms (59.07%) | Memory: 174.4 MB (9.07%) - LeetSync

--- 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
@@ -1,25 +1,24 @@
 class FoodRatings {
 public:
     unordered_map<string, string> cuisine; // food -> cuisine
-    unordered_map<string, set<pair<int, string> > > mp;
     unordered_map<string, int> rating; // food -> rate
+    unordered_map<string, set<pair<int, string> > > mp;
     FoodRatings(vector<string> &foods, vector<string> &cuisines, vector<int> &ratings) {
         for (int i = 0; i < cuisines.size(); i++) {
             cuisine[foods[i]] = cuisines[i];
-            mp[cuisines[i]].insert({ratings[i], foods[i]});
+            mp[cuisines[i]].insert({-ratings[i], foods[i]});
             rating[foods[i]] = ratings[i];
         }
     }
 
     void changeRating(string food, int newRating) {
-        mp[cuisine[food]].erase({rating[food], food});
-        mp[cuisine[food]].insert({newRating, food});
+        mp[cuisine[food]].erase({-rating[food], food});
+        mp[cuisine[food]].insert({-newRating, food});
         rating[food] = newRating;
     }
 
     string highestRated(string cuisine) {
-        int highestRate = mp[cuisine].rbegin()->first;
-        return mp[cuisine].lower_bound({highestRate, ""})->second;
+        return mp[cuisine].begin()->second;
     }
 };
 
