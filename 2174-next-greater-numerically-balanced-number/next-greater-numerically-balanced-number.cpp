9b944808ad809e484fe640a4711f960d5f914f24
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2025-10-24 14:58:49 +0300

    Time: 4 ms (76.38%) | Memory: 9.4 MB (50.25%) - LeetSync

---
+++ 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    // 1 22 333
+    int ans = 1e9;
+    void rec(int x, int n, vector<int> &cur) {
+        if (x == 10) {
+            if (cur.size() > 7) return;
+            // get all permutations
+            vector<int> gen = cur;
+            do {
+                int num = 0;
+                for (auto d: gen) {
+                    num = num * 10 + d;
+                }
+                if (num > n) ans = min(ans, num);
+                // all.push_back(num);
+            } while (next_permutation(begin(gen), end(gen)));
+            return;
+        }
+        rec(x + 1, n, cur);
+        if (cur.size() < 7) {
+            for (int i = 1; i <= x; i++) {
+                cur.push_back(x);
+            }
+            rec(x + 1, n, cur);
+            for (int i = 1; i <= x; i++) {
+                cur.pop_back();
+            }
+        }
+    }
+
+    int nextBeautifulNumber(int n) {
+        vector<int> v;
+        rec(1, n, v);
+        return ans;
+    }
+};
