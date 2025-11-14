fe14e75ea0d2603dacd0e2bb2ad3dba5764b76f7
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2025-11-14 15:16:15 +0200

    Time: 2367 ms (5.33%) | Memory: 91.5 MB (37.62%) - LeetSync

---
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
+        vector<vector<int>> pref(n, vector<int>(n));
+        for(auto query: queries) {
+            int x1 = query[0], y1 = query[1];
+            int x2 = query[2], y2 = query[3];
+            ++pref[x1][y1];
+            if(x2 + 1 < n and y2 + 1 < n)
+                ++pref[x2 + 1][y2 + 1];
+            if(y2 + 1 < n)
+                --pref[x1][y2 + 1];
+            if(x2 + 1 < n)
+                --pref[x2 + 1][y1];
+        }
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                cout << pref[i][j] <<" ";
+                pref[i][j] += i > 0 ? pref[i - 1][j] : 0;
+                pref[i][j] += j > 0 ? pref[i][j - 1] : 0;
+                pref[i][j] -= (i > 0 and j > 0 ? pref[i - 1][j - 1] : 0);
+            }
+            cout << endl;
+        }
+        return pref;
+    }
+};
