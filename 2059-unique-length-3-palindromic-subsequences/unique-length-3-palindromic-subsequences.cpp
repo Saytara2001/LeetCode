c0a607f495f2f652b2ffcbb157201b163a7dec82
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2025-11-21 13:39:44 +0200

    Time: 7 ms (99.33%) | Memory: 25.6 MB (12.81%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-
-        vector<vector<int>> idx(26, vector<int>(2, -1));
-        for(int i = 0; i < size(s); i++) {
-            if(idx[s[i]  - 'a'][0] == -1) {
-                idx[s[i] - 'a'][0] = i;
-            }else {
-                 idx[s[i] - 'a'][1] = i;
+        vector<vector<int>> v(26);
+        for(int i = 0; i < size(s) ; i++) {
+            v[s[i] - 'a'].push_back(i);
+        }
+        int uni = 0;
+        for(int c = 0; c < 26 ; c++) {
+            if(v[c].size() < 2)
+                continue;
+            int ft = v[c][0], lst = v[c].back();
+            for(int j = 0; j < 26; j++) {
+                auto it = upper_bound(begin(v[j]), end(v[j]), ft);
+                if(it != end(v[j]) and *it < lst)
+                    uni++;
             }
         }
-
-        int uniquePalindrome = 0;
-        vector<vector<bool>> vis(26, vector<bool>(26));
-        for(int i = 1; i < int(size(s)) - 1; i++) {
-            
-            for(char c = 'a'; c <= 'z'; c++) {
-                if(vis[s[i] - 'a'][c - 'a']) continue;
-                if(idx[c - 'a'][0] < i and idx[c - 'a'][1] > i) {
-                    vis[s[i] - 'a'][c - 'a'] = true;
-                    ++uniquePalindrome;
-                }
-            }
-        }
-
-        return uniquePalindrome;
+        return uni;
     }
 };
