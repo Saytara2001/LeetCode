d4eabf61892791f23bd231318a00af7f1fa20fc2
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2025-12-09 20:24:23 +0200

    Time: 935 ms (30.65%) | Memory: 427.8 MB (44.52%) - LeetSync

--- 3885-count-special-triplets/count-special-triplets.cpp
+++ 3885-count-special-triplets/count-special-triplets.cpp
@@ -3,22 +3,16 @@ public:
     int specialTriplets(vector<int>& nums) {
         int n = nums.size();
         const int mod = 1e9 + 7;
-        int max_val = *max_element(nums.begin(), nums.end());
-        vector<vector<int>> idx(max_val + 1);
-        for (int i = 0; i < n; i++) {
-            idx[nums[i]].push_back(i);
+        unordered_map<int, int> freq, pre;
+        for(auto x: nums) {
+            ++freq[x];
         }
-        int z = idx[0].size();
-        long long ans =
-            (1LL * z * (z - 1) * (z - 2) / 6) % mod; // combination of zeros
-
-        for (int val = 1; val <= max_val / 2; val++) {
-            for(auto j: idx[val]) {
-                int need = val * 2;
-                auto before = upper_bound(begin(idx[need]), end(idx[need]), j) - begin(idx[need]);
-                int after = idx[need].size() - before;
-                ans = (ans + 1LL * before * after % mod) % mod;
-            }
+        long long ans = 0;
+        for(auto x: nums) {
+            int need = 2 * x;
+            int after = freq[need] - pre[need] - (x == need);
+            ans = (ans + 1LL * pre[need] * after % mod) % mod;
+            ++pre[x];
         }
         return ans;
     }
