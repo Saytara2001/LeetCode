a115cc29ae4f3d84c8e16c6c02eb9eb4d5694fa5
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2025-12-27 11:02:47 +0200

    Time: 183 ms (18.54%) | Memory: 176.6 MB (33.67%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
     int maxTwoEvents(vector<vector<int>>& events) {
-        sort(events.begin(), events.end());
-        vector<vector<int>> dp(events.size(), vector<int>(3, -1));
-        return findEvents(events, 0, 0, dp);
-    }
-    // Recursive function to find the greatest sum for the pairs.
-    int findEvents(vector<vector<int>>& events, int idx, int cnt,
-                   vector<vector<int>>& dp) {
-        if (cnt == 2 || idx >= events.size()) return 0;
-        if (dp[idx][cnt] == -1) {
-            int end = events[idx][1];
-            int lo = idx + 1, hi = events.size() - 1;
-            while (lo < hi) {
-                int mid = lo + ((hi - lo) >> 1);
-                if (events[mid][0] > end)
-                    hi = mid;
-                else
-                    lo = mid + 1;
-            }
-            int include =
-                events[idx][2] + (lo < events.size() && events[lo][0] > end
-                                      ? findEvents(events, lo, cnt + 1, dp)
-                                      : 0);
-            int exclude = findEvents(events, idx + 1, cnt, dp);
-            dp[idx][cnt] = max(include, exclude);
+        sort(begin(events), end(events));
+        int n = events.size();
+        vector<int> suff(n + 1);
+        for (int i = n - 1; i >= 0; i--) {
+            suff[i] = max(suff[i + 1], events[i][2]);
         }
-        return dp[idx][cnt];
+        int maxScore = 0;
+        for (int i = 0; i < n; i++) {
+            maxScore = max(maxScore, events[i][2]);
+            vector<int> need{events[i][1], INT_MAX, INT_MAX};
+            auto idx =
+                upper_bound(begin(events), end(events), need) - begin(events);
+            if (idx < n) {
+                maxScore = max(maxScore, events[i][2] + suff[idx]);
+            }
+        }
+        return maxScore;
     }
 };
