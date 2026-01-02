e6ea2d240b22fc5bcb1549e56f4198c536f9a2a6
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2026-01-02 15:39:57 +0200

    Time: 0 ms (100.00%) | Memory: 28.4 MB (69.74%) - LeetSync

---
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        for (int k = 1; k <= 3; k++) {
+            for(int i = 0; i + k < nums.size(); i++) {
+                if(nums[i] == nums[i + k])
+                    return nums[i];
+            }
+        }
+        return -1;
+    }
+};
