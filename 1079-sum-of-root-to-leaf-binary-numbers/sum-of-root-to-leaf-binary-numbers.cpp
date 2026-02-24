6f9bbde69f40862dcd5349b7c696fd69d554215e
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2026-02-24 21:46:08 +0200

    Time: 0 ms (100.00%) | Memory: 17.2 MB (75.32%) - LeetSync

--- 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.cpp
+++ 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.cpp
@@ -6,30 +6,29 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    int dfs(TreeNode* root, string gen) {
-        if(root == NULL) {
+    int dfs(TreeNode* root, int x) {
+        if (root == NULL)
             return 0;
-        }
-        gen += char(root->val + '0');
-        if(root->left == nullptr and root->right == nullptr) {
-            int res = 0, n = gen.size();
-            for(int pow = 0; pow < n; pow++) {
-                res += (1 << pow) * (gen[n - pow - 1] - '0');
-            }
-            return res;
-        }
-        int left = dfs(root->left, gen);
-        int right = dfs(root->right, gen);
+
+        x <<= 1;
+        x |= root->val;
+
+        if (root->left == nullptr and root->right == nullptr)
+            return x;
+
+        int left = dfs(root->left, x);
+        int right = dfs(root->right, x);
 
         return left + right;
     }
     int sumRootToLeaf(TreeNode* root) {
-        string s = "";
-        return dfs(root, s);
+        int x = 0;
+        return dfs(root, x);
     }
 };
