c3d7366ca946a5dc2477f7586c65f6e1a7c9f6e6
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2026-03-14 14:10:23 +0200

    Time: 14 ms (43.15%) | Memory: 9.1 MB (58.31%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int cnt = 0;
     string res = "";
-    void rec(char prev, string s, int n, int k) {
+    void rec(string s, int n, int k) {
         if(s.size() == n) {
             ++cnt;
             if(cnt == k) res = s;
             return;
         }
         for(char x = 'a'; x <= 'c'; x++) {
-            if(prev != x) {
-                rec(x, s + x, n, k);
+            if(s.empty() or s.back() != x) {
+                rec(s + x, n, k);
             }
         }
     }
     string getHappyString(int n, int k) {
-        rec('z', "", n, k);
+        rec("", n, k);
         return res;
     }
 };
