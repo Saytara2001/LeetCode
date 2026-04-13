017b64aea4ad7f93f92b8b50a00fa1c3ff99d40a
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2026-04-13 19:52:20 +0200

    Time: 1116 ms (18.70%) | Memory: 223.3 MB (29.13%) - LeetSync

--- 2364-longest-path-with-different-adjacent-characters/longest-path-with-different-adjacent-characters.cpp
+++ 2364-longest-path-with-different-adjacent-characters/longest-path-with-different-adjacent-characters.cpp
@@ -11,22 +11,19 @@ public:
         vector<int> dis(n, 0);
         function<void(int, int)> dfs = [&](int node, int par) {
             dis[node] = 1;
+            int max1 = 0, max2 = 0;
             for (auto child: adj[node]) {
                 if (child == par) continue;
                 dfs(child, node);
                 if (s[node] != s[child]) {
                     dis[node] = max(dis[node], dis[child] + 1);
-                }
-            }
-            int max1 = 0, max2 = 0;
-            for (auto child: adj[node]) {
-                int cur = dis[child];
-                if(s[child] == s[node]) continue;
-                if(cur > max1) {
-                    max2 = max1;
-                    max1 = cur;
-                } else if(cur > max2) {
-                    max2 = cur;
+                    int cur = dis[child];
+                    if(cur > max1) {
+                        max2 = max1;
+                        max1 = cur;
+                    } else if(cur > max2) {
+                        max2 = cur;
+                    }
                 }
             }
             maxPath = max(maxPath, max1 + max2 + 1);
