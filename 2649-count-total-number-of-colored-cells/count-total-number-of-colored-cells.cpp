e0df9832b9c0dcb0f5cc0cc2f08258c9290f61ff
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2026-04-13 11:58:25 +0200

    Time: 0 ms (100.00%) | Memory: 7.8 MB (69.09%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        long long ans = 1;
-        for(int i = 1; i < n; i++) {
-            ans += 1LL * 4 * i;
-        }
-        return ans;
+        return 2LL * n * (n - 1) + 1;
     }
 };
