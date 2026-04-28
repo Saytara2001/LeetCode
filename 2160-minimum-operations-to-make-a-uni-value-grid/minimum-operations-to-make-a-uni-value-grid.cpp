57aca4f31b71c4962437d6d0c337f9af377d7576
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2026-04-28 09:21:23 +0300

    Time: 27 ms (78.60%) | Memory: 87.3 MB (62.96%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
-        vector<int> numsArray;
-        int result = 0;
-
-        // Flatten the grid into numsArray
-        for (int row = 0; row < grid.size(); row++) {
-            for (int col = 0; col < grid[0].size(); col++) {
-                numsArray.push_back(grid[row][col]);
+        int prev = -1;
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<int> all;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0;  j< m; j++) {
+                if(prev == -1) {
+                    prev = grid[i][j] % x;
+                } else {
+                    if(prev != grid[i][j] % x)
+                        return -1;
+                }
+                all.push_back(grid[i][j]);
             }
         }
-
-        int length = numsArray.size();
-
-        // Partially sorts the array so that the median element is placed at the
-        // middle index
-        nth_element(numsArray.begin(), numsArray.begin() + length / 2,
-                    numsArray.end());
-
-        // Store the median element as the final common value to make all
-        // elements equal to
-        int finalCommonNumber = numsArray[length / 2];
-
-        for (int number : numsArray) {
-            // If the remainder when divided by x is different for any number,
-            // return -1
-            if (number % x != finalCommonNumber % x) return -1;
-            // Add the number of operations required to make the current number
-            // equal to finalCommonNumber
-            result += abs(finalCommonNumber - number) / x;
+        sort(all.begin(), all.end());
+        int a = all[all.size() / 2];
+        int diff = 0;
+        for(int i = 0; i < all.size(); i++) {
+            diff += abs(all[i] - a) / x;
         }
-
-        return result;
+        return diff;
     }
 };
