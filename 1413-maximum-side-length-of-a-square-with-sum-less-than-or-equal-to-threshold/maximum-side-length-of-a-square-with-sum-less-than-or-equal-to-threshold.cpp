4deaca9923b60c346d19cba36b867a2a9330745d
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2026-04-29 16:05:38 +0300

    Time: 311 ms (11.19%) | Memory: 43.6 MB (6.93%) - LeetSync

---
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maxSideLength(vector<vector<int>>& mat, int threshold) {
+        int n = mat.size(), m = mat[0].size();
+        vector<vector<long long>> pref(n, vector<long long>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                pref[i][j] += mat[i][j];
+                pref[i][j] += i - 1 >= 0 ? pref[i - 1][j] : 0;
+                pref[i][j] += j - 1 >= 0 ? pref[i][j - 1] : 0;
+                pref[i][j] -=
+                    (i - 1 >= 0 and j - 1 >= 0 ? pref[i - 1][j - 1] : 0);
+            }
+        }
+        auto valid = [&](int mid) {
+            for (int i = 0; i + mid - 1 < n; i++) {
+                for (int j = 0; j + mid - 1 < m; j++) {
+                    int x2 = i + mid - 1, y2 = j + mid - 1;
+                    long long sum = pref[x2][y2];
+                    sum -= i - 1 >= 0 ? pref[i - 1][y2] : 0;
+                    sum -= j - 1 >= 0 ? pref[x2][j - 1] : 0;
+                    sum += (i - 1 >= 0 and j - 1 >= 0 ? pref[i - 1][j - 1] : 0);
+                    if (sum <= threshold)
+                        return true;
+                }
+            }
+            return false;
+        };
+        int mid = 300;
+        for (int i = 300; i >= 1; i--) {
+            if (valid(i))
+                return i;
+        }
+        return 0;
+    }
+};
