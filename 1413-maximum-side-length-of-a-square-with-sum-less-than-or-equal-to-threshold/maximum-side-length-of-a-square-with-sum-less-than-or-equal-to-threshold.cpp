de40203d4e478c7b30299a5713f5c45095c02e6b
Mohamed Hany <73343985+Saytara2001@users.noreply.github.com>
2026-04-29 16:08:23 +0300

    Time: 24 ms (41.56%) | Memory: 43.8 MB (6.04%) - LeetSync

--- 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -26,11 +26,17 @@ public:
             }
             return false;
         };
-        int mid = 300;
-        for (int i = 300; i >= 1; i--) {
-            if (valid(i))
-                return i;
+        int ans = 0;
+        int l = 1, r = min(n, m);
+        while (l <= r) {
+            int mid = (l + r) >> 1;
+            if (valid(mid)) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
         }
-        return 0;
+        return ans;
     }
 };
